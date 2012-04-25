76c1aed0a25c44b68d619f6910c3cd3a12f9fa58
Peter Rotich <peter@osticket.com>
2012-04-25 19:02:15 -0400

    Add transitional patch (devs)

--- main.inc.php
+++ main.inc.php
@@ -55,7 +55,7 @@
 
     #Current version && schema signature (Changes from version to version)
     define('THIS_VERSION','1.7-DPR2'); //Shown on admin panel
-    define('SCHEMA_SIGNATURE','bbb021fbeb377ca66b6997b77e0167cc'); //MD5 signature of the db schema. (used to trigger upgrades)
+    define('SCHEMA_SIGNATURE','49478749dc680eef08b7954bd568cfd1'); //MD5 signature of the db schema. (used to trigger upgrades)
 
     #load config info
     $configfile='';
