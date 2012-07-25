0b87ace9b46f98dc8ebeffded3c4daf4c313d04b
Peter Rotich <peter@osticket.com>
2012-07-25 12:04:50 -0400

    Add ability to overwrite (pre-declare) client's login page function

--- secure.inc.php
+++ secure.inc.php
@@ -16,9 +16,18 @@
 if(!strcasecmp(basename($_SERVER['SCRIPT_NAME']),basename(__FILE__))) die('Kwaheri!');
 if(!file_exists('client.inc.php')) die('Fatal Error.');
 require_once('client.inc.php');
+
+//Client Login page: Ajax interface can pre-declare the function to trap logins.
+if(!function_exists('clientLoginPage')) {
+    function clientLoginPage($msg ='') {
+        require('./login.php');
+        exit;
+    }
+}
+
 //User must be logged in!
 if(!$thisclient || !$thisclient->getId() || !$thisclient->isValid()){
-    require('./login.php');
+    clientLoginPage();
     exit;
 }
 $thisclient->refreshSession();
