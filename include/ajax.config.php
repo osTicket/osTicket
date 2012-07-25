7c631452f557854c6605446b07de584d742fb0d7
Peter Rotich <peter@osticket.com>
2012-07-25 11:58:57 -0400

    Add ajax suppor to client interface - needed to fetch multifile config.

--- include/ajax.config.php
+++ include/ajax.config.php
@@ -20,13 +20,26 @@ class ConfigAjaxAPI extends AjaxController {
 
     //config info UI might need.
     function scp() {
-        global $thisstaff, $cfg;
+        global $cfg;
 
-        $config=array('lock_time'       => ($cfg->getLockTime()*3600),
+        $config=array(
+                      'lock_time'       => ($cfg->getLockTime()*3600),
                       'file_types'      => $cfg->getAllowedFileTypes(),
                       'max_file_size'   => (int) $cfg->getMaxFileSize(),
                       'max_file_uploads'=> (int) $cfg->getStaffMaxFileUploads()
                       );
+        return $this->json_encode($config);
+    }
+
+    function client() {
+        global $cfg;
+
+        $config=array(
+                      'file_types'      => $cfg->getAllowedFileTypes(),
+                      'max_file_size'   => (int) $cfg->getMaxFileSize(),
+                      'max_file_uploads'=> (int) $cfg->getClientMaxFileUploads()
+                      );
+
         return $this->json_encode($config);
     }
 }
--- js/osticket.js
+++ js/osticket.js
@@ -1,1 +1,55 @@
-//Nothing for now...
+/* 
+   osticket.js
+   Copyright (c) osTicket.com
+ */
+
+$(document).ready(function(){
+
+    $("input:not(.dp):visible:enabled:first").focus();
+    $('table.list tbody tr:odd').addClass('odd');
+
+    $("form#save :input").change(function() {
+        var fObj = $(this).closest('form');
+        if(!fObj.data('changed')){
+            fObj.data('changed', true);
+            $('input[type=submit]', fObj).css('color', 'red');
+            $(window).bind('beforeunload', function(e) {
+                return 'Are you sure you want to leave? Any changes or info you\'ve entered will be discarded!';
+             });
+        }
+       });
+
+    $("form#save :input[type=reset]").click(function() {
+        var fObj = $(this).closest('form');
+        if(fObj.data('changed')){
+            $('input[type=submit]', fObj).removeAttr('style');
+            $('label', fObj).removeAttr('style');
+            $('label', fObj).removeClass('strike');
+            fObj.data('changed', false);
+            $(window).unbind('beforeunload');
+        }
+       });
+
+    $('form#save').submit(function() {
+        $(window).unbind('beforeunload');
+        return true;
+       });
+
+    /* Get config settings from the backend */
+    var $config = null;
+    $.ajax({
+        url: "ajax.php/config/client",
+        dataType: 'json',
+        async: false,
+        success: function (config) {
+            $config = config;
+            }
+        });
+     
+    /* Multifile uploads */
+     $('.multifile').multifile({
+        container:   '.uploads',
+        max_uploads: ($config && $config.max_file_uploads)?$config.max_file_uploads:1,
+        file_types:  ($config && $config.file_types)?$config.file_types:".*"
+       });
+});
