af0e9f80d7665f526d48f78c2d45da1551432764
Peter Rotich <peter@osticket.com>
2012-07-28 23:51:06 -0400

    Remove unnecessary 'array_merge'

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -1404,7 +1404,7 @@ class Ticket{
         $attachments = array();
         //Web based upload.. note that we're not "validating" attachments on response.
         if($_FILES['attachments'] && ($files=Format::files($_FILES['attachments'])))
-            $attachments=array_merge($attachments, $this->uploadAttachments($files, $respId,'R'));
+            $attachments=$this->uploadAttachments($files, $respId, 'R');
 
         //Canned attachments...
         if($vars['cannedattachments'] && is_array($vars['cannedattachments'])) {
