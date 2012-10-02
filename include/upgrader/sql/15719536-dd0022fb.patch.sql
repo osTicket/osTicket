27c382a33e7febb4f944b91b44aeb16015d3f9f2
Peter Rotich <peter@osticket.com>
2012-10-02 15:00:59 -0400

    Add transitional patch for internal (branch) installs

---
+++ include/upgrader/sql/15719536-dd0022fb.patch.sql
@@ -0,0 +1,11 @@
+
+/**
+ * @version v1.7 RC2+
+ * @signature dd0022fb14892c0bb6a9700392df2de7
+ *
+ * Transitional patch - dev branch installations
+ *  
+ */
+
+UPDATE `%TABLE_PREFIX%config`
+    SET `schema_signature`='dd0022fb14892c0bb6a9700392df2de7';
