2965125bf5ead72eaf1d9809431ee4da46b19a4c
Peter Rotich <peter@osticket.com>
2013-02-15 13:00:25 -0500

    Add overlay & loading spinner to client interface - shown on form submission to help cut down on double submits.

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -4,5 +4,10 @@
         <p>Copyright &copy; <?php echo date('Y'); ?> <a href="http://osticket.com" target="_blank" title="osTicket">osTicket.com</a> - All rights reserved.</p>
         <a id="poweredBy" href="http://osticket.com" target="_blank">Powered by osTicket</a>
     </div>
+<div id="overlay"></div>
+<div id="loading">
+    <h4>Please Wait!</h4>
+    <p>Please wait... it will take a second!</p>
+</div>    
 </body>
 </html>  
