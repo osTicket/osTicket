58d2995063023d97a1433d26d0fed135d0dba836
Jared Hancock <jared@osticket.com>
2013-05-23 10:59:22 -0500

    Migrate to new session storage model

--- include/upgrader/sql/d959a00e-892866ae.patch.sql
+++ include/upgrader/sql/d959a00e-892866ae.patch.sql
@@ -6,6 +6,10 @@
  * @signature 892866ae9af89d40415b738bbde54a15
  */
 
+ALTER TABLE `%TABLE_PREFIX%session`
+   CHANGE `session_id` `session_id` VARCHAR(255) collate ascii_general_ci,
+   CHANGE `session_data` `session_data` BLOB;
+
 -- update schema signature
 UPDATE `%TABLE_PREFIX%config`
     SET `schema_signature`='892866ae9af89d40415b738bbde54a15';
