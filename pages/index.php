d309b99b0ccb80440eb838fb540c840fc3bb0644
Jared Hancock <jared@osticket.com>
2013-07-09 17:48:12 -0400

    Allow generation and serving of abritrary custom pages
    The administrator can define pages in the admin panel defined as type
    'other', and when set to active, those pages can be served from the
    /pages/<page-name-slug> URL from the base of the helpdesk.
    
    This is mocked up only against Apache

---
+++ pages/index.php
@@ -0,0 +1,55 @@
+<?php
+/*********************************************************************
+    pages/index.php
+
+    Custom pages servlet
+
+    Peter Rotich <peter@osticket.com>
+    Jared Hancock <jared@osticket.com>
+    Copyright (c)  2006-2013 osTicket
+    http://www.osticket.com
+
+    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+    See LICENSE.TXT for details.
+
+    vim: expandtab sw=4 ts=4 sts=4:
+**********************************************************************/
+@chdir(realpath(dirname(__file__).'/../'));
+define('ROOT_PATH','../');
+
+require_once('client.inc.php');
+require_once(INCLUDE_DIR.'class.format.php');
+require_once(INCLUDE_DIR.'class.page.php');
+
+// Determine the requested page
+// - Strip extension
+$slug = Format::slugify($ost->get_path_info());
+
+// Get the part before the first dash
+$first_word = explode('-', $slug);
+$first_word = $first_word[0];
+
+$sql = 'SELECT id, name FROM '.PAGE_TABLE
+    .' WHERE name LIKE '.db_input("$first_word%");
+$page_id = null;
+
+$res = db_query($sql);
+while (list($id, $name) = db_fetch_row($res)) {
+    if (Format::slugify($name) == $slug) {
+        $page_id = $id;
+        break;
+    }
+}
+
+if (!$page_id || !($page = Page::lookup($page_id)))
+    Http::response(404, 'Page Not Found');
+
+if (!$page->isActive() || $page->getType() != 'other')
+    Http::response(404, 'Page Not Found');
+
+require(CLIENTINC_DIR.'header.inc.php');
+
+print $page->getBody();
+
+require(CLIENTINC_DIR.'footer.inc.php');
+?>
