49d871550c5cea8955c639ee285c51c6a537250f
Thomas Oster <thomas.oster@rwth-aachen.de>
2013-10-15 13:57:01 +0200

    Backend translation started

--- include/i18n/de_DE/lang.php
+++ include/i18n/de_DE/lang.php
@@ -1,12 +1,12 @@
 <?php
 $this->translations = array(
-    //header.inc.php
+    //client/header.inc.php
     "Guest User" => "Gast"
     ,"Log In" => "Einloggen"
     ,"Language" => "Sprache"
     ,"de_DE" => "deutsch"
     ,"en_US" => "englisch"
-    //login.inc.php
+    //client/login.inc.php
     ,"Check Ticket Status" => "Ticket Status überprüfen"
     ,"To view the status of a ticket, provide us with the login details below."
         => "Um den Status eines Tickets zu überprüfen geben Sie bitte unten Ihre Login Daten ein."
@@ -15,5 +15,22 @@ $this->translations = array(
     ,"View Status" => "Status anzeigen"
     ,"If this is your first time contacting us or you've lost the ticket ID, please %s open a new ticket%s."
         => "Falls Sie uns zum ersten Mal kontaktieren oder Sie Ihre Ticket ID verloren haben, bitte %s öffnen Sie ein neues Ticket%s."
+    //staff/login.tpl.php
+    ,"osTicket Staff Control Panel" => "osTicket Mitarbeiter-Seite"
+    ,"Session timed out due to inactivity" => "Sitzung abgelaufen"
+    //staff/header.inc.php
+    ,"Welcome, <strong>%s</strong>" => "Willkommen <strong>%s</strong>"
+    ,"My Preferences" => "Einstellungen"
+    ,"Log Out" => "Ausloggen"
+    ,"Admin Panel" => "Admin Seite"
+    ,"Dashboard" => "Pinnwand"
+    ,"Knowledgebase" => "Wissensdatenbank"
+    ,"Open (%d)" => "Offen (%d)"
+    ,"New Ticket" => "Neues Ticket anlegen"
+    ,"Closed Tickets (%d)" => "Geschlossene Tickets (%d)"
+    ,"My Closed Tickets (%d)" => "Meine geschlossenen Tickets (%d)"
+    ,'My Tickets (%d)' => "Meine Tickets (%d)"
+    ,"Answered (%d)" => "Beantwortet (%d)"
+    ,"Overdue (%d)" => "Überfällig (%d)"
 );
 ?>
--- include/staff/login.tpl.php
+++ include/staff/login.tpl.php
@@ -3,8 +3,8 @@ include_once(INCLUDE_DIR.'staff/login.header.php');
 $info = ($_POST && $errors)?Format::htmlchars($_POST):array();
 ?>
 <div id="loginBox">
-    <h1 id="logo"><a href="index.php">osTicket Staff Control Panel</a></h1>
-    <h3><?php echo Format::htmlchars($msg); ?></h3>
+    <h1 id="logo"><a href="index.php"><?php t("osTicket Staff Control Panel"); ?></a></h1>
+    <h3><?php echo Format::htmlchars(st($msg)); ?></h3>
     <form action="login.php" method="post">
         <?php csrf_token(); ?>
         <input type="hidden" name="do" value="scplogin">
@@ -13,9 +13,9 @@ $info = ($_POST && $errors)?Format::htmlchars($_POST):array();
             <input type="password" name="passwd" id="pass" placeholder="password" autocorrect="off" autocapitalize="off">
         </fieldset>
         <?php if ($_SESSION['_staff']['strikes'] > 1 && $cfg->allowPasswordReset()) { ?>
-        <h3 style="display:inline"><a href="pwreset.php">Forgot my password</a></h3>
+        <h3 style="display:inline"><a href="pwreset.php"><?php t("Forgot my password"); ?></a></h3>
         <?php } ?>
-        <input class="submit" type="submit" name="submit" value="Log In">
+        <input class="submit" type="submit" name="submit" value="<?php t("Log In"); ?>">
     </form>
 </div>
 <div id="copyRights">Copyright &copy; <a href='http://www.osticket.com' target="_blank">osTicket.com</a></div>
