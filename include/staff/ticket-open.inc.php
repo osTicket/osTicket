890cd6fc996d4e222b1a5768ef5344f0e7221c12
Jared Hancock <jared@osticket.com>
2013-11-04 18:27:52 +0000

    Fix validation error display on ticket open

--- include/staff/ticket-open.inc.php
+++ include/staff/ticket-open.inc.php
@@ -18,8 +18,8 @@ $info=Format::htmlchars(($errors && $_POST)?$_POST:$info);
     </thead>
     <tbody>
         <?php
-        $uf = UserForm::objects();
-        $uf[0]->render();
+        $uf = UserForm::getInstance();
+        $uf->render();
         if($cfg->notifyONNewStaffTicket()) {  ?>
         <tr>
             <td width="160">Alert:</td>
