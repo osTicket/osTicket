a171c44170a859b100b82e2a8edd34181abcb8a8
Jared Hancock <jared@osticket.com>
2013-11-14 15:40:46 -0600

    Regression: restore l.php Referrer cloaking

--- l.php
+++ l.php
@@ -23,6 +23,14 @@ if (!($url=trim($_GET['url'])))
 $check = (strpos($url, '//') === 0) ? 'http:' . $url : $url;
 if (!Validator::is_url($check) || !$ost->validateLinkToken($_GET['auth']))
     Http::response(403, 'URL link not authorized');
-else
+elseif (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false)
     Http::redirect($url);
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="refresh" content="0;URL=<?php echo $url; ?>"/>
+</head>
+<body/>
+</html>
