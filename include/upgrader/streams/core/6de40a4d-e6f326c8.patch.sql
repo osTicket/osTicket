b027bc2d2865d3e63a996f27c51e315616b98e51
slm4996 <slm4996+git@gmail.com>
2014-01-15 14:17:34 -0500

    Revolving SLA ignores answered messages

--- include/upgrader/streams/core/6de40a4d-e6f326c8.patch.sql
+++ include/upgrader/streams/core/6de40a4d-e6f326c8.patch.sql
@@ -1,5 +1,15 @@
 /**
- *This patch adds the isrevolvign column to the sla table
-*/
-ALTER TABLE '%TABLE_PREFIX%sla'  ADD COLUMN 'isrevolving' tinyint(1) NOT NULL default '0';
+ * @version 1.8-stable
+ * @signature e6f326c8e9863cd0c63c3c78d344fc37
+ *
+ * Add isrevolving column to sla table
+ */
 
+ALTER TABLE `%TABLE_PREFIX%config` 
+	ADD COLUMN `isrevolving` tinyint(1) unsigned NOT NULL default '0' 
+	AFTER `isactive`; 
+
+-- Finished with patch
+UPDATE `%TABLE_PREFIX%config`
+    SET `value` = 'e6f326c8e9863cd0c63c3c78d344fc37'
+    WHERE `key` = 'schema_signature' AND `namespace` = 'core';
