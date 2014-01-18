7a9f8665a0a4dab49c78eccdf90bd91a9a174bfe
Jared Hancock <jared@osticket.com>
2014-01-18 00:07:17 -0600

    Fixup AttachmentFile::migrate()

--- include/class.file.php
+++ include/class.file.php
@@ -386,7 +386,7 @@ class AttachmentFile {
         $after = hash_init('sha1');
 
         // Copy the file to the new backend and hash the contents
-        $target = AttachmentStorageBackend::lookup($bk, $this->ht);
+        $target = FileStorageBackend::lookup($bk, $this);
         $source = $this->open();
         // TODO: Make this resumable so that if the file cannot be migrated
         //      in the max_execution_time, the migration can be continued
@@ -398,7 +398,7 @@ class AttachmentFile {
 
         // Verify that the hash of the target file matches the hash of the
         // source file
-        $target = AttachmentStorageBackend::lookup($bk, $this->ht);
+        $target = FileStorageBackend::lookup($bk, $this);
         while ($block = $target->read())
             hash_update($after, $block);
 
