d98ab76ddcccc9df1f104e7667a9364d017706fc
slm4996 <slm4996+git@gmail.com>
2014-01-24 08:13:44 -0500

    Hide close/hide dialog when esc key is pressed

--- scp/js/scp.js
+++ scp/js/scp.js
@@ -371,6 +371,13 @@ $(document).ready(function(){
         return false;
     });
 
+    $(document).keydown(function(e) {        
+        if (e.keyCode == 27) {
+            $('div.dialog').hide();
+            $('#overlay').hide();
+        }  
+    });
+
     /* advanced search */
     $('.dialog#advanced-search').css({
         top  : ($(window).height() / 6),
