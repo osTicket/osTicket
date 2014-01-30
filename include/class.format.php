e0c7e4b66dbe5f75b9d97506a4ee3aaf9704e2e1
Jared Hancock <jared@osticket.com>
2014-01-30 13:15:06 -0600

    oops: Make files viewable with _ in key

--- include/class.format.php
+++ include/class.format.php
@@ -385,7 +385,7 @@ class Format {
     }
 
     function viewableImages($html, $script='image.php') {
-        return preg_replace_callback('/"cid:([\\w.-]{32})"/',
+        return preg_replace_callback('/"cid:([\w.-_]{32})"/',
         function($match) use ($script) {
             $hash = $match[1];
             if (!($file = AttachmentFile::lookup($hash)))
