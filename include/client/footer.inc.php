705a611caf3b21198ce9609ebbfe1e552b24bd33
Jared Hancock <jared@osticket.com>
2014-02-27 15:58:57 -0600

    lint: Fix bad merge conflict

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -1,7 +1,7 @@
         </div>
     </div>
     <div id="footer">
-        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company :? 'osTicket.com'; ?> - All rights reserved.</p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - All rights reserved.</p>
         <a id="poweredBy" href="http://osticket.com" target="_blank">Helpdesk software - powered by osTicket</a>
     </div>
 <div id="overlay"></div>
