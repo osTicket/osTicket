osTicket/osTicket

commit 9199989dec8099fe4daf2f224b1bd6b5a66fd682

oops: No reply separator on diagnostic email

include/class.mailer.php
@@ -157,8 +157,9 @@ class Mailer {
         $mid_token = (isset($options['thread']))
             ? $options['thread']->asMessageId($to) : '';
         if (!(isset($options['text']) && $options['text'])) {
-            if($cfg->stripQuotedReply() && ($tag=$cfg->getReplySeparator()))
-                $message = "<div style=\"display:none\" data-mid=\"$mid_token\">$tag</div>"
+            if ($cfg->stripQuotedReply() && ($tag=$cfg->getReplySeparator())
+                    && (!isset($options['reply-tag']) || $options['reply-tag']))
+                $message = "<div style=\"display:none\" data-mid=\"$mid_token\">$tag<br/><br/></div
                     .$message;
             // Make sure nothing unsafe has creeped into the message
             $message = Format::safe_html($message); //XXX??

scp/emailtest.php
@@ -36,7 +36,8 @@ if($_POST){
 
     if(!$errors && $email){
         if($email->send($_POST['email'],$_POST['subj'],
-                Format::sanitize($_POST['message']))) {
+                Format::sanitize($_POST['message']),
+                null, array('reply-tag'=>false))) {
             $msg='Test email sent successfully to '.Format::htmlchars($_POST['email']);
             Draft::deleteForNamespace('email.diag');
         }

commit 1ff51dc2efc26e88af86452cecdb407088128338

oops: Fix syntax error introduced by 9199989de

include/class.mailer.php
@@ -159,8 +159,8 @@ class Mailer {
         if (!(isset($options['text']) && $options['text'])) {
             if ($cfg->stripQuotedReply() && ($tag=$cfg->getReplySeparator())
                     && (!isset($options['reply-tag']) || $options['reply-tag']))
-                $message = "<div style=\"display:none\" data-mid=\"$mid_token\">$tag<br/><br/></div
-                    .$message;
+                $message = "<div style=\"display:none\"
+                    data-mid=\"$mid_token\">$tag<br/><br/></div>$message";
             // Make sure nothing unsafe has creeped into the message
             $message = Format::safe_html($message); //XXX??
             $txtbody = rtrim(Format::html2text($message, 90, false))