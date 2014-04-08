bd3100dd5d5839680bece48bd71d1aa438d060e7
Jared Hancock <jared@osticket.com>
2014-04-08 10:34:11 -0500

    Only attempt email link sign-on if tokens exist
    If the `view.php` page is requested and an auth token is not present in the
    URL, do not attempt to sign the user on since it will always fail and will
    result in a false "Failed login attempt" warning to the system log.

--- view.php
+++ view.php
@@ -19,7 +19,9 @@ require_once('client.inc.php');
 // Try autologin the user
 // Authenticated user can be of type ticket owner or collaborator
 $errors = array();
-$user =  UserAuthenticationBackend::processSignOn($errors);
+if (isset($_GET['auth']) || isset($_GET['t']))
+    $user =  UserAuthenticationBackend::processSignOn($errors);
+
 if ($user && $user->getTicketId())
     Http::redirect('tickets.php?id='.$user->getTicketId());
 
