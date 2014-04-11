0c32eabbbb83ab9d4e76495a5ec6c2fa6acefa38
Jared Hancock <jared@osticket.com>
2014-04-11 17:21:03 -0500

    Fixup merging of email recipients
    This stems from a confusing similarity between the + operator for arrays
    and array_merge() in php. Adding arrays will ignore items in the RHS where
    keys are present in the LHS. Therefore, when adding numerically indexed
    arrays together, only items on the RHS that have a key higher than the
    greatest key on the LHS will be included.

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -1132,7 +1132,7 @@ class Ticket {
                 $recipients[] = $assignee;
         } elseif(!strcasecmp(get_class($assignee), 'Team')) {
             if($cfg->alertTeamMembersONAssignment() && ($members=$assignee->getMembers()))
-                $recipients+=$members;
+                $recipients = array_merge($recipients, $members);
             elseif($cfg->alertTeamLeadONAssignment() && ($lead=$assignee->getTeamLead()))
                 $recipients[] = $lead;
         }
@@ -1388,11 +1388,11 @@ class Ticket {
                 if($this->getStaffId())
                     $recipients[]=$this->getStaff();
                 elseif($this->getTeamId() && ($team=$this->getTeam()) && ($members=$team->getMembers()))
-                    $recipients+=$members;
+                    $recipients = array_merge($recipients, $members);
             } elseif($cfg->alertDeptMembersONTransfer() && !$this->isAssigned()) {
                 //Only alerts dept members if the ticket is NOT assigned.
                 if(($members=$dept->getMembers()))
-                    $recipients+=$members;
+                    $recipients = array_merge($recipients, $members);
             }
 
             //Always alert dept manager??
