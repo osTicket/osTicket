c18045af41398ea7c0d91911884ae800e663663c
Jared Hancock <jared@osticket.com>
2014-04-29 09:38:46 -0500

    notes: Hide new note box when editing a note

--- include/staff/templates/notes.tmpl.php
+++ include/staff/templates/notes.tmpl.php
@@ -5,8 +5,10 @@ foreach ($notes as $note) {
     include STAFFINC_DIR."templates/note.tmpl.php";
 } ?>
 </div>
+<div id="new-note-box">
 <div class="quicknote" id="new-note" data-url="<?php echo $create_note_url; ?>">
 <div class="body">
     <a href="#"><i class="icon-plus icon-large"></i> &nbsp; Click to create a new note</a>
+</div>
 </div>
 </div>
