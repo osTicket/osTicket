ae0340f1c25bbf7e6e9c4855f649f9adf4bdfa3a
raj2ras <raj@3dfool.com>
2014-06-19 16:02:35 -0700

    customized
    customized

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -1,8 +1,8 @@
         </div>
     </div>
     <div id="footer">
-        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - All rights reserved.</p>
-        <a id="poweredBy" href="http://osticket.com" target="_blank">Helpdesk software - powered by osTicket</a>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'summitaec.com'; ?> - All rights reserved.</p>
+        <a id="poweredBy" href="http://www.summitaec.com" target="_blank">Helpdesk software - powered by SummitAEC</a>
     </div>
 <div id="overlay"></div>
 <div id="loading">
--- include/staff/footer.inc.php
+++ include/staff/footer.inc.php
@@ -1,3 +1,4 @@
+<?php if (!isset($_SERVER['HTTP_X_PJAX'])) { ?>
     </div>
     <div id="footer">
         Copyright &copy; 2006-<?php echo date('Y'); ?>&nbsp;<?php echo (string) $ost->company ?: 'osTicket.com'; ?>&nbsp;All Rights Reserved.
@@ -12,13 +13,26 @@ if(is_object($thisstaff) && $thisstaff->isStaff()) { ?>
 <?php
 } ?>
 </div>
+</div>
 <div id="overlay"></div>
 <div id="loading">
-    <h4>Please Wait!</h4>
-    <p>Please wait... it will take a second!</p>
+    <i class="icon-spinner icon-spin icon-3x pull-left icon-light"></i>
+    <h1>Loading ...</h1>
 </div>
 <div class="dialog" style="display:none;width:650px;" id="popup">
     <div class="body"></div>
 </div>
+<script type="text/javascript">
+if ($.support.pjax) {
+  $(document).on('click', 'a', function(event) {
+    if (!$(this).hasClass('no-pjax')
+        && !$(this).closest('.no-pjax').length
+        && $(this).attr('href')[0] != '#')
+      $.pjax.click(event, {container: $('#pjax-container'), timeout: 2000});
+  })
+}
+</script>
 </body>
 </html>
+<?php } # endif X_PJAX ?>
+
