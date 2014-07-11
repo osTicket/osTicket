89ef30d7cab6d2f4d226954682eaf09c399eb2fb
Jared Hancock <jared@osticket.com>
2014-07-11 08:33:21 -0500

    i18n: Add concept of translatable content

--- include/ajax.i18n.php
+++ include/ajax.i18n.php
@@ -37,5 +37,80 @@ class i18nAjaxAPI extends AjaxController {
         Http::cacheable(md5($data), $cfg->lastModified());
         echo $data;
     }
+
+    function getTranslations($tag) {
+        $t = CustomDataTranslation::allTranslations($tag);
+        $phrases = array();
+        $lm = 0;
+        foreach ($t as $translation) {
+            $phrases[$translation->lang] = $translation->text;
+            $lm = max($lm, strtotime($translation->updated));
+        }
+        $json = JsonDataEncoder::encode($phrases) ?: '{}';
+        //Http::cacheable(md5($json), $lm);
+
+        return $json;
+    }
+
+    function updateTranslations($tag) {
+        global $thisstaff, $cfg;
+
+        if (!$thisstaff)
+            Http::response(403, "Agent login required");
+        if (!$_POST)
+            Http::response(422, "No translations found to update");
+
+        $t = CustomDataTranslation::allTranslations($tag);
+        $phrases = array();
+        foreach ($t as $translation) {
+            $phrases[$translation->lang] = $translation;
+        }
+        foreach ($_POST as $lang => $phrase) {
+            if (isset($phrases[$lang])) {
+                if (!$phrase) {
+                    $p->delete();
+                }
+                else {
+                    $p = $phrases[$lang];
+                    // Avoid XSS injection
+                    $p->text = trim(Format::striptags($phrase));
+                    $p->agent_id = $thisstaff->getId();
+                }
+            }
+            elseif (in_array($lang, $cfg->getSecondaryLanguages())) {
+                if (!$phrase)
+                    continue;
+                $phrases[$lang] = CustomDataTranslation::create(array(
+                    'lang'          => $lang,
+                    'text'          => $phrase,
+                    'object_hash'   => $tag,
+                    'type'          => 'phrase',
+                    'agent_id'      => $thisstaff->getId(),
+                    'updated'       => new SqlFunction('NOW'),
+                ));
+            }
+            else {
+                Http::response(400,
+                    sprintf("%s: Must be a secondary language", $lang));
+            }
+        }
+        // Commit.
+        foreach ($phrases as $p)
+            if (!$p->save())
+                Http::response(500, sprintf("%s: Unable to commit language"));
+    }
+
+    function getSecondaryLanguages() {
+        global $cfg;
+
+        $langs = array();
+        foreach (array('de', 'ja', 'zh_CN') as $l) {
+            $langs[$l] = Internationalization::getLanguageDescription($l);
+        }
+        $json = JsonDataEncoder::encode($langs);
+        Http::cacheable(md5($json), $cfg->lastModified());
+
+        return $json;
+    }
 }
 ?>
