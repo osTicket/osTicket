2d7b52b4deda0a5bcdb30eca57f15a171cf4da6d
Luc Didry <luc@didry.org>
2014-08-03 20:53:20 +0200

    Trad : include/client/knowledgebase.inc.php

--- include/client/knowledgebase.inc.php
+++ include/client/knowledgebase.inc.php
@@ -2,13 +2,13 @@
 if(!defined('OSTCLIENTINC')) die('Access Denied');
 
 ?>
-<h1>Frequently Asked Questions</h1>
+<h1>Foire Aux Questions</h1>
 <form action="index.php" method="get" id="kb-search">
     <input type="hidden" name="a" value="search">
     <div>
         <input id="query" type="text" size="20" name="q" value="<?php echo Format::htmlchars($_REQUEST['q']); ?>">
         <select name="cid" id="cid">
-            <option value="">&mdash; All Categories &mdash;</option>
+            <option value="">&mdash; Toutes les catégories &mdash;</option>
             <?php
             $sql='SELECT category_id, name, count(faq.category_id) as faqs '
                 .' FROM '.FAQ_CATEGORY_TABLE.' cat '
@@ -31,7 +31,7 @@ if(!defined('OSTCLIENTINC')) die('Access Denied');
     </div>
     <div>
         <select name="topicId" id="topic-id">
-            <option value="">&mdash; All Help Topics &mdash;</option>
+            <option value="">&mdash; Tous les articles d'aide &mdash;</option>
             <?php
             $sql='SELECT ht.topic_id, CONCAT_WS(" / ", pht.topic, ht.topic) as helptopic, count(faq.topic_id) as faqs '
                 .' FROM '.TOPIC_TABLE.' ht '
@@ -79,19 +79,19 @@ if($_REQUEST['q'] || $_REQUEST['cid'] || $_REQUEST['topicId']) { //Search.
     }
 
     $sql.=' GROUP BY faq.faq_id ORDER BY question';
-    echo "<div><strong>Search Results</strong></div><div class='clear'></div>";
+    echo "<div><strong>Résultats de recherche</strong></div><div class='clear'></div>";
     if(($res=db_query($sql)) && ($num=db_num_rows($res))) {
-        echo '<div id="faq">'.$num.' FAQs matched your search criteria.
+        echo '<div id="faq">'.$num.' FAQs qui correspondent à votre recherche.
                 <ol>';
         while($row=db_fetch_array($res)) {
             echo sprintf('
                 <li><a href="faq.php?id=%d" class="previewfaq">%s</a></li>',
-                $row['faq_id'],$row['question'],$row['ispublished']?'Published':'Internal');
+                $row['faq_id'],$row['question'],$row['ispublished']?'Publié':'Interne');
         }
         echo '  </ol>
              </div>';
     } else {
-        echo '<strong class="faded">The search did not match any FAQs.</strong>';
+        echo '<strong class="faded">La recherche n\'a pas trouvé de FAQ qui corresponde à votre recherche.</strong>';
     }
 } else { //Category Listing.
     $sql='SELECT cat.category_id, cat.name, cat.description, cat.ispublic, count(faq.faq_id) as faqs '
@@ -102,7 +102,7 @@ if($_REQUEST['q'] || $_REQUEST['cid'] || $_REQUEST['topicId']) { //Search.
         .' HAVING faqs>0 '
         .' ORDER BY cat.name';
     if(($res=db_query($sql)) && db_num_rows($res)) {
-        echo '<div>Click on the category to browse FAQs.</div>
+        echo '<div>Cliquez sur la catégorie pour parcourir la FAQ.</div>
                 <ul id="kb">';
         while($row=db_fetch_array($res)) {
 
@@ -117,7 +117,7 @@ if($_REQUEST['q'] || $_REQUEST['cid'] || $_REQUEST['topicId']) { //Search.
         }
         echo '</ul>';
     } else {
-        echo 'NO FAQs found';
+        echo 'Aucune FAQ trouvée';
     }
 }
 ?>
