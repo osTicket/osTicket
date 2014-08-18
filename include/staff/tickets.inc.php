45bf1633f9bf0a82d2d181d4d31f87a0e205e792
Jared Hancock <jared@osticket.com>
2014-08-18 10:03:24 -0500

    Apply numeric sort to the ticket number column

--- include/staff/tickets.inc.php
+++ include/staff/tickets.inc.php
@@ -146,7 +146,7 @@ if ($_REQUEST['advsid'] && isset($_SESSION['adv_'.$_REQUEST['advsid']])) {
         db_input($_SESSION['adv_'.$_REQUEST['advsid']])).')';
 }
 
-$sortOptions=array('date'=>'effective_date','ID'=>'ticket.`number`',
+$sortOptions=array('date'=>'effective_date','ID'=>'ticket.`number`*1',
     'pri'=>'pri.priority_urgency','name'=>'user.name','subj'=>'cdata.subject',
     'status'=>'ticket.status','assignee'=>'assigned','staff'=>'staff',
     'dept'=>'dept.dept_name');
