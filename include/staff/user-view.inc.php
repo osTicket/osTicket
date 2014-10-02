1b143b3c796e7f12704854ae91e9763e6fa1b9a3
Jared Hancock <jared@osticket.com>
2014-10-02 16:32:56 -0500

    i18n: rtl: Fix quick note layout for rtl display

--- include/staff/user-view.inc.php
+++ include/staff/user-view.inc.php
@@ -14,8 +14,8 @@ $org = $user->getOrganization();
         </td>
         <td width="50%" class="right_align has_bottom_border">
             <span class="action-button pull-right" data-dropdown="#action-dropdown-more">
+                <i class="icon-caret-down pull-right"></i>
                 <span ><i class="icon-cog"></i> <?php echo __('More'); ?></span>
-                <i class="icon-caret-down"></i>
             </span>
             <a id="user-delete" class="action-button pull-right user-action"
             href="#users/<?php echo $user->getId(); ?>/delete"><i class="icon-trash"></i>
