5355cc7e93adb448b8bdbedf1e1c5ce23e21964e
Jared Hancock <jared@osticket.com>
2014-10-17 23:42:31 -0500

    draft: Put inline image draft race to rest
    The solution is simple: upload the image, create the draft automatically,
    attach the image to the draft, return the draft_id to the client. That's it.
    There's no more need to create the draft in advance now.

--- ajax.php
+++ ajax.php
@@ -33,6 +33,7 @@ $dispatcher = patterns('',
         url_post('^(?P<id>\d+)$', 'updateDraftClient'),
         url_delete('^(?P<id>\d+)$', 'deleteDraftClient'),
         url_post('^(?P<id>\d+)/attach$', 'uploadInlineImageClient'),
+        url_post('^(?P<namespace>[\w.]+)/attach$', 'uploadInlineImageEarlyClient'),
         url_get('^(?P<namespace>[\w.]+)$', 'getDraftClient'),
         url_post('^(?P<namespace>[\w.]+)$', 'createDraftClient')
     )),
