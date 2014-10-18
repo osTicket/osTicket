11c6914ff36d0a4a9776761e9f9a39b14ae90dde
Jared Hancock <jared@osticket.com>
2014-10-17 23:04:55 -0500

    kb: Complete new layout for translatable kb

--- include/client/faq-category.inc.php
+++ include/client/faq-category.inc.php
@@ -1,34 +1,64 @@
 <?php
 if(!defined('OSTCLIENTINC') || !$category || !$category->isPublic()) die('Access Denied');
 ?>
-<h1><strong><?php echo $category->getName() ?></strong></h1>
+
+<div class="row">
+<div class="span8">
+    <h1><?php echo __('Frequently Asked Questions');?></h1>
+    <h2><strong><?php echo $category->getLocalName() ?></strong></h2>
 <p>
-<?php echo Format::safe_html($category->getDescription()); ?>
+<?php echo Format::safe_html($category->getLocalDescriptionWithImages()); ?>
 </p>
 <hr>
 <?php
-$sql='SELECT faq.faq_id, question, count(attach.file_id) as attachments '
-    .' FROM '.FAQ_TABLE.' faq '
-    .' LEFT JOIN '.ATTACHMENT_TABLE.' attach
-         ON(attach.object_id=faq.faq_id AND attach.type=\'F\' AND attach.inline = 0) '
-    .' WHERE faq.ispublished=1 AND faq.category_id='.db_input($category->getId())
-    .' GROUP BY faq.faq_id '
-    .' ORDER BY question';
-if(($res=db_query($sql)) && db_num_rows($res)) {
+$faqs = FAQ::objects()
+    ->filter(array('category'=>$category))
+    ->exclude(array('ispublished'=>false))
+    ->annotate(array('has_attachments'=>Aggregate::COUNT('attachments', false,
+        array('attachments__inline'=>0))))
+    ->order_by('-ispublished', 'question');
+
+if ($faqs->exists(true)) {
     echo '
-         <h2>'.__('Frequently Asked Questions').'</h2>
+         <h2>'.__('Further Articles').'</h2>
          <div id="faq">
             <ol>';
-    while($row=db_fetch_array($res)) {
-        $attachments=$row['attachments']?'<span class="Icon file"></span>':'';
+foreach ($faqs as $F) {
+        $attachments=$F->has_attachments?'<span class="Icon file"></span>':'';
         echo sprintf('
             <li><a href="faq.php?id=%d" >%s &nbsp;%s</a></li>',
-            $row['faq_id'],Format::htmlchars($row['question']), $attachments);
+            $F->getId(),Format::htmlchars($F->question), $attachments);
     }
     echo '  </ol>
-         </div>
-         <p><a class="back" href="index.php">&laquo; '.__('Go Back').'</a></p>';
+         </div>';
 }else {
     echo '<strong>'.__('This category does not have any FAQs.').' <a href="index.php">'.__('Back To Index').'</a></strong>';
 }
 ?>
+</div>
+
+<div class="span4">
+    <div class="sidebar">
+    <div class="searchbar">
+        <form method="get" action="faq.php">
+        <input type="hidden" name="a" value="search"/>
+        <input type="text" name="q" class="search" placeholder="<?php
+            echo __('Search our knowledge base'); ?>"/>
+        <input type="submit" style="display:none" value="search"/>
+        </form>
+    </div>
+    <div class="content">
+        <section>
+            <div class="header"><?php echo __('Help Topics'); ?></div>
+<?php
+foreach (Topic::objects()
+    ->filter(array('faqs__faq__category__category_id'=>$category->getId()))
+    as $t) { ?>
+        <a href="?topicId=<?php echo urlencode($t->getId()); ?>"
+            ><?php echo $t->getFullname(); ?></a>
+<?php } ?>
+        </section>
+    </div>
+    </div>
+</div>
+</div>
--- include/client/knowledgebase.inc.php
+++ include/client/knowledgebase.inc.php
@@ -2,95 +2,33 @@
 if(!defined('OSTCLIENTINC')) die('Access Denied');
 
 ?>
-<h1><?php echo __('Frequently Asked Questions');?></h1>
-<form action="index.php" method="get" id="kb-search">
-    <input type="hidden" name="a" value="search">
-    <div>
-    <input id="query" type="text" size="20" name="q" value="<?php echo Format::htmlchars($_REQUEST['q']); ?>">
-        <input id="searchSubmit" type="submit" value="<?php echo __('Search');?>">
-    </div>
-    <div class="sidebar">
-        <select name="topicId" id="topic-id">
-            <option value="">&mdash; <?php echo __('All Help Topics');?> &mdash;</option>
-            <?php
-foreach (Topic::objects()
-        ->annotate(array('faqs_count'=>Aggregate::count('faqs')))
-        ->filter(array('faqs_count__gt'=>0))
-    as $t) {
-                echo sprintf('<option value="%d" %s>%s</option>',
-                    $t->getId(),
-                    ($_REQUEST['topicId'] && $t->getId() == $_REQUEST['topicId']?'selected="selected"':''),
-                    $t->getFullName());
-            }
-            ?>
-        </select>
-    </div>
-</form>
-<hr>
-<div>
 <?php
-if($_REQUEST['q'] || $_REQUEST['cid'] || $_REQUEST['topicId']) { //Search.
-    $sql='SELECT faq.faq_id, question '
-        .' FROM '.FAQ_TABLE.' faq '
-        .' LEFT JOIN '.FAQ_CATEGORY_TABLE.' cat ON(cat.category_id=faq.category_id) '
-        .' LEFT JOIN '.FAQ_TOPIC_TABLE.' ft ON(ft.faq_id=faq.faq_id) '
-        .' WHERE faq.ispublished=1 AND cat.ispublic=1';
+if($_REQUEST['q'] || $_REQUEST['cid'] || $_REQUEST['topicId']) { //Search
+    $faqs = FAQ::allPublic()
+        ->annotate(array(
+            'attachment_count'=>Aggregate::COUNT('attachments'),
+            'topic_count'=>Aggregate::COUNT('topics')
+        ))
+        ->order_by('question');
 
-    if($_REQUEST['cid'])
-        $sql.=' AND faq.category_id='.db_input($_REQUEST['cid']);
+    if ($_REQUEST['cid'])
+        $faqs->filter(array('category_id'=>$_REQUEST['cid']));
 
-    if($_REQUEST['topicId'])
-        $sql.=' AND ft.topic_id='.db_input($_REQUEST['topicId']);
+    if ($_REQUEST['topicId'])
+        $faqs->filter(array('topics__topic_id'=>$_REQUEST['topicId']));
 
+    if ($_REQUEST['q'])
+        $faqs->filter(Q::ANY(array(
+            'question__contains'=>$_REQUEST['q'],
+            'answer__contains'=>$_REQUEST['q'],
+            'keywords__contains'=>$_REQUEST['q'],
+            'category__name__contains'=>$_REQUEST['q'],
+            'category__description__contains'=>$_REQUEST['q'],
+        )));
 
-    if($_REQUEST['q']) {
-        $sql.=" AND (question LIKE ('%".db_input($_REQUEST['q'],false)."%')
-                 OR answer LIKE ('%".db_input($_REQUEST['q'],false)."%')
-                 OR keywords LIKE ('%".db_input($_REQUEST['q'],false)."%')
-                 OR cat.name LIKE ('%".db_input($_REQUEST['q'],false)."%')
-                 OR cat.description LIKE ('%".db_input($_REQUEST['q'],false)."%')
-                 )";
-    }
+    include CLIENTINC_DIR . 'kb-search.inc.php';
 
-    $sql.=' GROUP BY faq.faq_id ORDER BY question';
-    echo "<div><strong>".__('Search Results').'</strong></div><div class="clear"></div>';
-    if(($res=db_query($sql)) && ($num=db_num_rows($res))) {
-        echo '<div id="faq">'.sprintf(__('%d FAQs matched your search criteria.'),$num).'
-                <ol>';
-        while($row=db_fetch_array($res)) {
-            echo sprintf('
-                <li><a href="faq.php?id=%d" class="previewfaq">%s</a></li>',
-                $row['faq_id'],$row['question'],$row['ispublished']?__('Published'):__('Internal'));
-        }
-        echo '  </ol>
-             </div>';
-    } else {
-        echo '<strong class="faded">'.__('The search did not match any FAQs.').'</strong>';
-    }
 } else { //Category Listing.
-    $categories = Category::objects()
-        ->filter(array('ispublic'=>true, 'faqs__ispublished'=>true))
-        ->annotate(array('faq_count'=>Aggregate::count('faqs')))
-        ->filter(array('faq_count__gt'=>0));
-    if ($categories->all()) {
-        echo '<div>'.__('Click on the category to browse FAQs.').'</div>
-                <ul id="kb">';
-
-        foreach ($categories as $C) { ?>
-            <li><i></i>
-            <h4><?php echo sprintf('<a href="faq.php?cid=%d">%s (%d)</a>',
-                $C->getId(), Format::htmlchars($C->name), $C->faq_count); ?></h4>
-                <?php echo Format::safe_html($C->description); ?>
-<?php       foreach ($C->faqs->order_by('-view')->limit(5) as $F) { ?>
-                <div class="popular-faq"><?php echo $F->question; ?></div>
-<?php       } ?>
-            </li>
-<?php   } ?>
-       </ul>
-<?php
-    } else {
-        echo __('NO FAQs found');
-    }
+    include CLIENTINC_DIR . 'kb-categories.inc.php';
 }
 ?>
-</div>
