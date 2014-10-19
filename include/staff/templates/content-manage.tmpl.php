f4b7dd28527a00402deda5990a523b124fafcd9c
Jared Hancock <jared@osticket.com>
2014-10-18 22:55:06 -0500

    i18n: Add vertical tabs to the content management dialog

--- include/staff/templates/content-manage.tmpl.php
+++ include/staff/templates/content-manage.tmpl.php
@@ -1,30 +1,25 @@
 <h3><?php echo __('Manage Content'); ?> &mdash; <?php echo Format::htmlchars($content->getName()); ?></h3>
 <a class="close" href=""><i class="icon-remove-circle"></i></a>
 <hr/>
-<?php if ($langs) { ?>
-<div class="banner">&nbsp;
-    <span class="pull-left">
-        <i class="icon-globe icon-large"></i>
-        <?php echo __('This content is translatable'); ?>
-    </span>
-    <span class="pull-right">
-        <select onchange="javascript:
-    $('option', this).each(function(){$($(this).val()).hide()});
-    $($('option:selected', this).val()).show(); ">
-            <option value="#reference-text"><?php echo
-            Internationalization::getLanguageDescription($cfg->getPrimaryLanguage());
-            ?> — <?php echo __('Primary'); ?></option>
-<?php   foreach ($langs as $tag) { ?>
-            <option value="#translation-<?php echo $tag; ?>"><?php echo
-            Internationalization::getLanguageDescription($tag);
-            ?></option>
-<?php   } ?>
-        </select>
-    </span>
-</div>
+
+<form method="post" action="#content/<?php echo $content->getId(); ?>"
+        style="clear:none">
+<?php
+if (count($langs) > 1) { ?>
+    <ul class="vertical left tabs">
+        <li class="empty"><i class="icon-globe" title="This content is translatable"></i></li>
+<?php foreach ($langs as $tag=>$nfo) { ?>
+    <li class="<?php if ($tag == $cfg->getPrimaryLanguage()) echo "active";
+        ?>"><a href="#translation-<?php echo $tag; ?>" title="<?php
+        echo Internationalization::getLanguageDescription($tag);
+    ?>"><span class="flag flag-<?php echo strtolower($nfo['flag']); ?>"></span>
+    </a></li>
 <?php } ?>
-<form method="post" action="#content/<?php echo $content->getId(); ?>">
-    <div id="reference-text" lang="<?php echo $cfg->getPrimaryLanguage(); ?>">
+    </ul>
+<?php
+} ?>
+    <div id="translation-<?php echo $cfg->getPrimaryLanguage(); ?>"
+        class="tab_content left-tabs" style="padding:0" lang="<?php echo $cfg->getPrimaryLanguage(); ?>">
     <input type="text" style="width: 100%; font-size: 14pt" name="name" value="<?php
         echo Format::htmlchars($content->getName()); ?>" />
     <div style="margin-top: 5px">
@@ -34,21 +29,29 @@
     </div>
     </div>
 
-<?php foreach ($langs as $tag) { ?>
-    <div id="translation-<?php echo $tag; ?>" style="display:none" lang="<?php echo $tag; ?>">
-    <input type="text" style="width: 100%; font-size: 14pt" name="trans[<?php echo $tag; ?>][title]" value="<?php
-        echo Format::htmlchars($info['title'][$tag]); ?>" />
+<?php foreach ($langs as $tag=>$nfo) {
+        if ($tag == $cfg->getPrimaryLanguage())
+            continue; ?>
+    <div id="translation-<?php echo $tag; ?>" class="tab_content left-tabs"
+        style="display:none;padding:0" dir="<?php echo $nfo['direction']; ?>" lang="<?php echo $tag; ?>">
+    <input type="text" style="width: 100%; font-size: 14pt"
+        name="trans[<?php echo $tag; ?>][title]" value="<?php
+        echo Format::htmlchars($info['title'][$tag]); ?>"
+        placeholder="<?php echo __('Title'); ?>" />
     <div style="margin-top: 5px">
-    <textarea class="richtext no-bar" name="trans[<?php echo $tag; ?>][body]"><?php
+    <textarea class="richtext no-bar" data-direction=<?php echo $nfo['direction']; ?>
+        placeholder="<?php echo __('Message content'); ?>"
+        name="trans[<?php echo $tag; ?>][body]"><?php
     echo Format::htmlchars(Format::viewableImages($info['body'][$tag]));
 ?></textarea>
     </div>
     </div>
 <?php } ?>
 
-    <div id="msg_info" style="margin-top:7px"><?php
+    <div class="info-banner left-tabs" style="margin-top:7px"><?php
 echo $content->getNotes(); ?></div>
-    <hr/>
+
+    <hr class="clear"/>
     <p class="full-width">
         <span class="buttons pull-left">
             <input type="reset" value="<?php echo __('Reset'); ?>">
@@ -59,6 +62,6 @@ echo $content->getNotes(); ?></div>
             <input type="submit" value="<?php echo __('Save Changes'); ?>">
         </span>
      </p>
-</form>
 </div>
+</form>
 <div class="clear"></div>
