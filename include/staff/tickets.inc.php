76a24a78a6f7ec5359bb5d92ede69b1cdcbca793
Chefkeks <Chefkeks@users.noreply.github.com>
2014-10-29 11:27:44 +0100

    Update tickets.inc.php

--- include/staff/tickets.inc.php
+++ include/staff/tickets.inc.php
@@ -449,7 +449,8 @@ if ($results) {
                 </td>
                 <?php } ?>
                 <td title="<?php echo $row['email']; ?>" nowrap>
-                  <a class="Icon <?php echo strtolower($row['source']); ?>Ticket ticketPreview" title="Preview Ticket"
+                  <a class="Icon <?php echo strtolower($row['source']); ?>Ticket ticketPreview"
+                    title="<?php echo __('Preview Ticket'); ?>"
                     href="tickets.php?id=<?php echo $row['ticket_id']; ?>"><?php echo $tid; ?></a></td>
                 <td align="center" nowrap><?php echo Format::db_datetime($row['effective_date']); ?></td>
                 <td><a <?php if ($flag) { ?> class="Icon <?php echo $flag; ?>Ticket" title="<?php echo ucfirst($flag); ?> Ticket" <?php } ?>
