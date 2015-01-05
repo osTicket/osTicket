osTicket/osTicket

commit bebc2d792f5648917ca7d98225f5ae649f57241d

tabs: Make tabs more generic and dynamic

include/staff/templates/user-account.tmpl.php
@@ -15,8 +15,7 @@ if ($info['error']) {
 } elseif ($info['msg']) {
     echo sprintf('<p id="msg_notice">%s</p>', $info['msg']);
 } ?>
-<form method="post" class="user" action="#users/<?php echo $user->getId(); ?>/manage" >
-<ul class="tabs">
+<ul class="tabs" id="user-account-tabs">
     <li <?php echo !$access? 'class="active"' : ''; ?>><a href="#user-account"
         ><i class="icon-user"></i>&nbsp;<?php echo __('User Information'); ?></a></li>
     <li <?php echo $access? 'class="active"' : ''; ?>><a href="#user-access"
@@ -25,6 +24,7 @@ if ($info['error']) {
 
 
  <input type="hidden" name="id" value="<?php echo $user->getId(); ?>" />
+<div id="user-account-tabs_container">
  <div class="tab_content"  id="user-account" style="display:<?php echo $access? 'none' : 'block'; ?>; margin:5px;">
     <form method="post" class="user" action="#users/<?php echo $user->getId(); ?>/manage" >
         <input type="hidden" name="id" value="<?php echo $user->getId(); ?>" />
@@ -149,6 +149,7 @@ if ($info['error']) {
             </tr>
         </tbody>
         </table>
+   </div>
    </div>
    <hr>
    <p class="full-width">

include/staff/dynamic-list.inc.php
@@ -27,7 +27,7 @@ $info=Format::htmlchars(($errors && $_POST) ? array_merge($info,$_POST) : $info)
     <h2><?php echo __('Custom List'); ?>
     <?php echo $list ? $list->getName() : 'Add new list'; ?></h2>
 
-<ul class="tabs">
+<ul class="tabs" id="list-tabs">
     <li class="active"><a href="#definition">
         <i class="icon-plus"></i> <?php echo __('Definition'); ?></a></li>
     <li><a href="#items">
@@ -35,7 +35,7 @@ $info=Format::htmlchars(($errors && $_POST) ? array_merge($info,$_POST) : $info)
     <li><a href="#properties">
         <i class="icon-asterisk"></i> <?php echo __('Properties'); ?></a></li>
 </ul>
-
+<div id="list-tabs_container">
 <div id="definition" class="tab_content">
     <table class="form_table" width="940" border="0" cellspacing="0" cellpadding="2">
     <thead>
@@ -108,7 +108,7 @@ $info=Format::htmlchars(($errors && $_POST) ? array_merge($info,$_POST) : $info)
     </tbody>
     </table>
 </div>
-<div id="properties" class="tab_content" style="display:none">
+<div id="properties" class="hidden tab_content">
     <table class="form_table" width="940" border="0" cellspacing="0" cellpadding="2">
     <thead>
         <tr>
@@ -211,7 +211,7 @@ $info=Format::htmlchars(($errors && $_POST) ? array_merge($info,$_POST) : $info)
     </tbody>
 </table>
 </div>
-<div id="items" class="tab_content" style="display:none">
+<div id="items" class="hidden tab_content">
     <table class="form_table" width="940" border="0" cellspacing="0" cellpadding="2">
     <thead>
     <?php if ($list) {
@@ -330,6 +330,7 @@ $info=Format::htmlchars(($errors && $_POST) ? array_merge($info,$_POST) : $info)
     }?>
     </tbody>
     </table>
+</div>
 </div>
 <p class="centered">
     <input type="submit" name="submit" value="<?php echo $submit_text; ?>">

include/staff/org-view.inc.php
@@ -63,31 +63,33 @@ if(!defined('OSTSCPINC') || !$thisstaff || !is_object($org)) die('Invalid path')
 </table>
 <br>
 <div class="clear"></div>
-<ul class="tabs">
-    <li class="active"><a id="users_tab" href="#users"><i
+<ul class="tabs" id="orgtabs">
+    <li class="active"><a href="#users"><i
     class="icon-user"></i>&nbsp;<?php echo __('Users'); ?></a></li>
-    <li><a id="tickets_tab" href="#tickets"><i
+    <li><a href="#tickets"><i
     class="icon-list-alt"></i>&nbsp;<?php echo __('Tickets'); ?></a></li>
-    <li><a id="notes_tab" href="#notes"><i
+    <li><a href="#notes"><i
     class="icon-pushpin"></i>&nbsp;<?php echo __('Notes'); ?></a></li>
 </ul>
+<div id="orgtabs_container">
 <div class="tab_content" id="users">
 <?php
 include STAFFINC_DIR . 'templates/users.tmpl.php';
 ?>
 </div>
-<div class="tab_content" id="tickets"  style="display:none;">
+<div class="hidden tab_content" id="tickets">
 <?php
 include STAFFINC_DIR . 'templates/tickets.tmpl.php';
 ?>
 </div>
 
-<div class="tab_content" id="notes" style="display:none">
+<div class="hidden tab_content" id="notes">
 <?php
 $notes = QuickNote::forOrganization($org);
 $create_note_url = 'orgs/'.$org->getId().'/note';
 include STAFFINC_DIR . 'templates/notes.tmpl.php';
 ?>
+</div>
 </div>
 
 <script type="text/javascript">

include/staff/templates/org-profile.tmpl.php
@@ -13,16 +13,16 @@ if ($info['error']) {
 } elseif ($info['msg']) {
     echo sprintf('<p id="msg_notice">%s</p>', $info['msg']);
 } ?>
-<ul class="tabs">
-    <li class="active"><a href="#tab-profile"
+<ul class="tabs" id="orgprofile">
+    <li class="active"><a href="#profile"
         ><i class="icon-edit"></i>&nbsp;<?php echo __('Fields'); ?></a></li>
     <li><a href="#contact-settings"
         ><i class="icon-fixed-width icon-cogs faded"></i>&nbsp;<?php
         echo __('Settings'); ?></a></li>
 </ul>
 <form method="post" class="org" action="<?php echo $action; ?>">
-
-<div class="tab_content" id="tab-profile" style="margin:5px;">
+<div id="orgprofile_container">
+<div class="tab_content" id="profile" style="margin:5px;">
 <?php
 $action = $info['action'] ? $info['action'] : ('#orgs/'.$org->getId());
 if ($ticket && $ticket->getOwnerId() == $user->getId())
@@ -38,7 +38,7 @@ if ($ticket && $ticket->getOwnerId() == $user->getId())
     </table>
 </div>
 
-<div class="tab_content" id="contact-settings" style="display:none;margin:5px;">
+<div class="hidden tab_content" id="contact-settings" style="margin:5px;">
     <table style="width:100%">
         <tbody>
             <tr>
@@ -139,7 +139,7 @@ if ($ticket && $ticket->getOwnerId() == $user->getId())
         </tbody>
     </table>
 </div>
-
+</div>
 <div class="clear"></div>
 
 <hr>

include/staff/templates/ticket-preview.tmpl.php
@@ -29,7 +29,7 @@ elseif($msg)
 elseif($warn)
     echo sprintf('<div id="msg_warning">%s</div>',$warn);
 
-echo '<ul class="tabs">';
+echo '<ul class="tabs" id="ticket-preview">';
 
 echo '
         <li class="active"><a id="preview_tab" href="#preview"
@@ -42,7 +42,7 @@ echo sprintf('
             $ticket->getNumCollaborators());
 }
 echo '</ul>';
-
+echo '<div id="ticket-preview_container">';
 echo '<div class="tab_content" id="preview">';
 echo '<table border="0" cellspacing="" cellpadding="1" width="100%" class="ticket_info">';
 
@@ -117,7 +117,7 @@ echo '
     </table>';
 echo '</div>'; // ticket preview content.
 ?>
-<div class="tab_content" id="collab" style="display:none;">
+<div class="hidden tab_content" id="collab">
     <table border="0" cellspacing="" cellpadding="1">
         <colgroup><col style="min-width: 250px;"></col></colgroup>
         <?php
@@ -145,6 +145,7 @@ echo '</div>'; // ticket preview content.
                                 ? __('Manage Collaborators') : __('Add Collaborator')
                                 );
     ?>
+</div>
 </div>
 <?php
 $options = array();

include/staff/templates/user-import.tmpl.php
@@ -10,6 +10,12 @@ if ($info['error']) {
 } elseif ($info['msg']) {
     echo sprintf('<p id="msg_notice">%s</p>', $info['msg']);
 } ?>
+<ul class="tabs" id="user-import-tabs">
+    <li class="active"><a href="#copy-paste"
+        ><i class="icon-edit"></i>&nbsp;<?php echo __('Copy Paste'); ?></a></li>
+    <li><a href="#upload"
+        ><i class="icon-fixed-width icon-cloud-upload"></i>&nbsp;<?php echo __('Upload'); ?></a></li>
+</ul>
 <form action="<?php echo $info['action']; ?>" method="post" enctype="multipart/form-data"
     onsubmit="javascript:
     if ($(this).find('[name=import]').val()) {
@@ -26,7 +32,7 @@ if ($info['error']) {
 if ($org_id) { ?>
     <input type="hidden" name="id" value="<?php echo $org_id; ?>"/>
 <?php } ?>
-
+<div id="user-import-tabs_container">
 <div class="tab_content" id="copy-paste" style="margin:5px;">
 <h2 style="margin-bottom:10px"><?php echo __('Name and Email'); ?></h2>
 <p><?php echo __(
@@ -39,7 +45,7 @@ if ($org_id) { ?>
 </textarea>
 </div>
 
-<div class="tab_content" id="upload" style="display:none;margin:5px;">
+<div class="hidden tab_content" id="upload" style="margin:5px;">
 <h2 style="margin-bottom:10px"><?php echo __('Import a CSV File'); ?></h2>
 <p>
 <em><?php echo sprintf(__(
@@ -71,6 +77,7 @@ if ($org_id) { ?>
 </tr></table>
 <br/>
 <input type="file" name="import"/>
+</div>
 </div>
     <hr>
     <p class="full-width">

include/staff/templates/user.tmpl.php
@@ -34,11 +34,11 @@ if ($info['error']) {
     } ?>
 
 <div class="clear"></div>
-<ul class="tabs" style="margin-top:5px">
+<ul class="tabs" id="user_tabs" style="margin-top:5px">
     <li class="active"><a href="#info-tab"
         ><i class="icon-info-sign"></i>&nbsp;<?php echo __('User'); ?></a></li>
 <?php if ($org) { ?>
-    <li><a href="#organization-tab"
+    <li><a href="#org-tab"
         ><i class="icon-fixed-width icon-building"></i>&nbsp;<?php echo __('Organization'); ?></a></li>
 <?php }
     $ext_id = "U".$user->getId();
@@ -47,6 +47,7 @@ if ($info['error']) {
         ><i class="icon-fixed-width icon-pushpin"></i>&nbsp;<?php echo __('Notes'); ?></a></li>
 </ul>
 
+<div id="user_tabs_container">
 <div class="tab_content" id="info-tab">
 <div class="floating-options">
     <a href="<?php echo $info['useredit'] ?: '#'; ?>" id="edituser" class="action" title="<?php echo __('Edit'); ?>"><i class="icon-edit"></i></a>
@@ -70,7 +71,7 @@ if ($info['error']) {
 </div>
 
 <?php if ($org) { ?>
-<div class="tab_content" id="organization-tab" style="display:none">
+<div class="hidden tab_content" id="org-tab">
 <div class="floating-options">
     <a href="orgs.php?id=<?php echo $org->getId(); ?>" title="<?php
     echo __('Manage Organization'); ?>" class="action"><i class="icon-share"></i></a>
@@ -92,7 +93,7 @@ if ($info['error']) {
 </div>
 <?php } # endif ($org) ?>
 
-<div class="tab_content" id="notes-tab" style="display:none">
+<div class="hidden tab_content" id="notes-tab">
 <?php $show_options = true;
 foreach ($notes as $note)
     include STAFFINC_DIR . 'templates/note.tmpl.php';
@@ -103,6 +104,7 @@ foreach ($notes as $note)
 <div class="body">
     <a href="#"><i class="icon-plus icon-large"></i> &nbsp;
     <?php echo __('Click to create a new note'); ?></a>
+</div>
 </div>
 </div>
 </div>

include/staff/ticket-view.inc.php
@@ -376,8 +376,8 @@ foreach (DynamicFormEntry::forTicket($ticket->getId()) as $form) {
 $tcount = $ticket->getThreadCount();
 $tcount+= $ticket->getNumNotes();
 ?>
-<ul id="threads">
-    <li><a class="active" id="toggle_ticket_thread" href="#"><?php echo sprintf(__('Ticket Thread (%d)'), $tcount); ?></a></li>
+<ul  class="tabs threads" id="ticket_tabs" >
+    <li><a class="active" href="#ticket_thread"><?php echo sprintf(__('Ticket Thread (%d)'), $tcount); ?></a></li>
 </ul>
 <div id="ticket_tabs_container">
     <div id="ticket_thread" class="tab_content">
@@ -436,7 +436,6 @@ $tcount+= $ticket->getNumNotes();
     } else {
         echo '<p>'.__('Error fetching ticket thread - get technical help.').'</p>';
     }?>
-</div>
 <div class="clear" style="padding-bottom:10px;"></div>
 <?php if($errors['err']) { ?>
     <div id="msg_error"><?php echo $errors['err']; ?></div>
@@ -453,7 +452,7 @@ $tcount+= $ticket->getNumNotes();
         <li class="active"><a href="#reply"><?php echo __('Post Reply');?></a></li>
         <?php
         } ?>
-        <li><a id="note_tab" href="#note"><?php echo __('Post Internal Note');?></a></li>
+        <li><a href="#note"><?php echo __('Post Internal Note');?></a></li>
         <?php
         if ($role->canTransferTickets()) { ?>
         <li><a href="#transfer"><?php echo __('Department Transfer');?></a></li>
@@ -579,9 +578,9 @@ $tcount+= $ticket->getNumNotes();
     echo $attrs; ?>><?php echo $draft ?: $info['response'];
                     ?></textarea>
                 <div id="reply_form_attachments" class="attachments">
-<?php
-print $response_form->getField('attachments')->render();
-?>
+                <?php
+                    print $response_form->getField('attachments')->render();
+                ?>
                 </div>
                 </td>
             </tr>
@@ -655,7 +654,8 @@ print $response_form->getField('attachments')->render();
     </form>
     <?php
     } ?>
-    <form id="note" class="tab_content" action="tickets.php?id=<?php echo $ticket->getId(); ?>#note" name="note" method="post" enctype="multipart/form-data">
+    <form id="note" class="hidden tab_content" action="tickets.php?id=<?php
+        echo $ticket->getId(); ?>#note" name="note" method="post" enctype="multipart/form-data">
         <?php csrf_token(); ?>
         <input type="hidden" name="id" value="<?php echo $ticket->getId(); ?>">
         <input type="hidden" name="locktime" value="<?php echo $cfg->getLockTime(); ?>">
@@ -692,9 +692,9 @@ print $response_form->getField('attachments')->render();
     echo $attrs; ?>><?php echo $draft ?: $info['note'];
                         ?></textarea>
                 <div class="attachments">
-<?php
-print $note_form->getField('attachments')->render();
-?>
+                <?php
+                    print $note_form->getField('attachments')->render();
+                ?>
                 </div>
                 </td>
             </tr>
@@ -893,6 +893,8 @@ print $note_form->getField('attachments')->render();
     </form>
     <?php
     } ?>
+ </div>
+ </div>
 </div>
 <div style="display:none;" class="dialog" id="print-options">
     <h3><?php echo __('Ticket Print Options');?></h3>

commit 3323e0138de2e9f88019b1b00b4b035856f1d8b2

oops: Fix a few small issues
  * Fix missing attachments join for Category (fixes #1654)
  * Fix crash on user account registration
  * Fix save button on user account (fixes #1655) — regression from bebc2d7

include/class.attachment.php
@@ -104,6 +104,13 @@ class AttachmentModel extends VerySimpleModel {
     );
 }
 
+class GenericAttachment extends VerySimpleModel {
+    static $meta = array(
+        'table' => ATTACHMENT_TABLE,
+        'pk' => array('id'),
+    );
+}
+
 class GenericAttachments {
 
     var $id;

include/class.client.php
@@ -319,8 +319,6 @@ class  EndUser extends BaseAuthenticatedUser {
 
 class ClientAccount extends UserAccount {
 
-    var $_extra;
-
     function checkPassword($password, $autoupdate=true) {
 
         /*bcrypt based password match*/
@@ -395,12 +393,12 @@ class ClientAccount extends UserAccount {
             }
         }
 
-        if (!$vars['timezone_id'])
-            $errors['timezone_id']=__('Time zone selection is required');
+        // Timezone selection is not required. System default is a valid
+        // fallback
 
         if ($errors) return false;
 
-        $this->set('timezone_id', $vars['timezone_id']);
+        $this->set('timezone', $vars['timezone']);
         $this->set('dst', isset($vars['dst']) ? 1 : 0);
         // Change language
         $this->set('lang', $vars['lang'] ?: null);

include/class.faq.php
@@ -29,14 +29,15 @@ class FAQ extends VerySimpleModel {
                 'constraint' => array(
                     'category_id' => 'Category.category_id'
                 ),
-            ), /* XXX: Not yet implemented
+            ),
             'attachments' => array(
                 'constraint' => array(
                     "'F'" => 'GenericAttachment.type',
                     'faq_id' => 'GenericAttachment.object_id',
                 ),
                 'list' => true,
-            ), */
+                'null' => true,
+            ),
             'topics' => array(
                 'constraint' => array(
                     'faq_id' => 'FaqTopic.faq_id'

include/class.user.php
@@ -788,6 +788,7 @@ class UserAccountModel extends VerySimpleModel {
     );
 
     var $_status;
+    var $_extra;
 
     function __construct() {
         call_user_func_array(array('parent', '__construct'), func_get_args());
@@ -860,7 +861,7 @@ class UserAccountModel extends VerySimpleModel {
 
     function getExtraAttr($attr=false, $default=null) {
         if (!isset($this->_extra))
-            $this->_extra = JsonDataParser::decode($this->extra);
+            $this->_extra = JsonDataParser::decode($this->get('extra', ''));
 
         return $attr ? (@$this->_extra[$attr] ?: $default) : $this->_extra;
     }
@@ -984,8 +985,8 @@ class UserAccount extends UserAccountModel {
 
         // TODO: Make sure the username is unique
 
-        if (!$vars['timezone'])
-            $errors['timezone'] = __('Time zone selection is required');
+        // Timezone selection is not required. System default is a valid
+        // fallback
 
         // Changing password?
         if ($vars['passwd1'] || $vars['passwd2']) {

include/staff/templates/user-account.tmpl.php
@@ -15,6 +15,7 @@ if ($info['error']) {
 } elseif ($info['msg']) {
     echo sprintf('<p id="msg_notice">%s</p>', $info['msg']);
 } ?>
+<form method="post" class="user" action="#users/<?php echo $user->getId(); ?>/manage" >
 <ul class="tabs" id="user-account-tabs">
     <li <?php echo !$access? 'class="active"' : ''; ?>><a href="#user-account"
         ><i class="icon-user"></i>&nbsp;<?php echo __('User Information'); ?></a></li>