a394f7cd6fe131057c3228b43f7d4c6388f3fba8
Jared Hancock <jared@osticket.com>
2015-02-11 13:47:16 -0600

    login: Require CSRF token to login
    This patch fixes a vulnerable scenario, where sequential login attempts can
    be made without an existing session, and without a valid CSRF token. This
    scenario lends itself well for brute force password attempts, because
    attackers can avoid using a session and still send requests to determine if
    a set of credentials are valid. This vector also avoids the authentication
    lockout mechanism, because it requires an ongoing session to shutdown the
    requests.
    
    This patch addresses the issue by requiring a session and a valid CSRF token
    generated by the server and placed in the session to be submitted with the
    credentials. Therefore, an existing session and a Cookie header are required
    to process a login attempt. Secondly, the CSRF token will be changed on the
    server after each login processed. Therefore, for each session, a subsequent
    GET request would be necessary before submitting another login attempt.

--- login.php
+++ login.php
@@ -25,7 +25,19 @@ require_once(INCLUDE_DIR.'class.client.php');
 require_once(INCLUDE_DIR.'class.ticket.php');
 
 $inc = 'login.inc.php';
+
+// Check the CSRF token, and ensure that future requests will have to use a
+// different CSRF token. This will help ward off both parallel and serial
+// brute force attacks, because new tokens will have to be requested for
+// each attempt.
 if ($_POST) {
+    // Check CSRF token
+    if (!$ost->checkCSRFToken())
+        Http::response(400, __('Valid CSRF Token Required'));
+
+    // Rotate the CSRF token (original cannot be reused)
+    $ost->getCSRF()->rotate();
+
     if (!$_POST['lticket'] || !Validator::is_email($_POST['lemail']))
         $errors['err'] = 'Valid email address and ticket number required';
     elseif (($user = UserAuthenticationBackend::process($_POST['lemail'],
