8258b32ab9b20b0c9bb7e0ef145f3500023c468e
Jared Hancock <jared@osticket.com>
2015-03-06 14:11:57 -0600

    cli: Add beginnings of endpoints for cron items

---
+++ setup/cli/modules/cron.php
@@ -0,0 +1,34 @@
+<?php
+require_once dirname(__file__) . "/class.module.php";
+require_once dirname(__file__) . "/../cli.inc.php";
+
+class CronManager extends Module {
+    var $prologue = 'CLI cron manager for osTicket';
+
+    var $arguments = array(
+        'action' => array(
+            'help' => 'Action to be performed',
+            'options' => array(
+                'fetch' => 'Fetch email',
+                'search' => 'Build search index'
+            ),
+        ),
+    );
+
+    function run($args, $options) {
+        Bootstrap::connect();
+        $ost = osTicket::start();
+
+        switch (strtolower($args[0])) {
+        case 'fetch':
+            Cron::MailFetcher();
+            break;
+        case 'search':
+            $ost->searcher->backend->IndexOldStuff();
+            break;
+        }
+    }
+}
+
+Module::register('cron', 'CronManager');
+?>
