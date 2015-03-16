bd2d0000c72e11df0ceed2191836f27fe971f528
Jared Hancock <jared@osticket.com>
2015-03-16 17:29:10 -0500

    files: Clear attachment box after posting a response

--- tickets.php
+++ tickets.php
@@ -90,7 +90,7 @@ if($_POST && is_object($ticket) && $ticket->getId()):
                 Draft::deleteForNamespace('ticket.client.' . $ticket->getId());
                 // Drop attachments
                 $attachments->reset();
-                $tform->setSource(array());
+                $attachments->getForm()->setSource(array());
             } else {
                 $errors['err']=__('Unable to post the message. Try again');
             }
