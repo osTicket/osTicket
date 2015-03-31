00341802c23d3af47e3282f6127407b71ce4dc73
Jared Hancock <jared@osticket.com>
2015-03-31 11:14:51 -0500

    i18n: Ensure UTF-8 is used as the request encoding

--- include/staff/header.inc.php
+++ include/staff/header.inc.php
@@ -1,4 +1,6 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
