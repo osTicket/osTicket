ad9276e987121fb1c1fe1416199b4c7b8d2b8c87
Jared Hancock <jared@osticket.com>
2015-04-13 11:27:31 -0500

    charset: Consider case-insensitive charset names
    Also fix bug where bogus charset names like `iso` were confused with valid
    charset names starting with that text, such as `iso-8859-2`.

--- include/class.charset.php
+++ include/class.charset.php
@@ -24,16 +24,16 @@ class Charset {
         $match = array();
         switch (true) {
         // Windows charsets - force correct format
-        case preg_match('`^Windows-?(\d+)$`', $charset, $match):
+        case preg_match('`^Windows-?(\d+)$`i', $charset, $match):
             return 'Windows-'.$match[1];
         // ks_c_5601-1987: Korean alias for cp949
-        case preg_match('`^ks_c_5601-1987`', $charset):
+        case preg_match('`^ks_c_5601-1987`i', $charset):
             return 'cp949';
-        case preg_match('`^iso-?(\S+)$`', $charset, $match):
+        case preg_match('`^iso-?(\S+)$`i', $charset, $match):
             return "ISO-".$match[1];
         // Incorrect, bogus, ambiguous or empty charsets
         // ISO-8859-1 is assumed
-        case preg_match('`^(default|x-user-defined|iso|us-ascii)`', $charset):
+        case preg_match('`^(default|x-user-defined|iso|us-ascii)$`i', $charset):
         case preg_match('`^\s*$`', $charset):
             return 'ISO-8859-1';
         }
