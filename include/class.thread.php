ed4c570672550930096e46ed30f713ef372ab365
hawkhead99 <scott.brazelton@gmail.com>
2015-05-11 12:33:08 -0500

    Accounting for when staff has no user id
    This commit accounts for when the staff member has no user id

--- include/class.thread.php
+++ include/class.thread.php
@@ -993,9 +993,13 @@ class ThreadEntry {
                 //Lookup by ticket number
                 && ($ticket = Ticket::lookupByNumber($match[1]))
                 //Lookup the user using the email address
-                && ($user = User::lookup(array('emails__address' => $mailinfo['email'])))) {
+                && (($user = User::lookup(array('emails__address' => $mailinfo['email'])))
+                    || ($staff = Staff::getIdByEmail($mailinfo['email'])))) {
            
-            $staff = Staff::getIdByEmail($mailinfo['email']);
+            //staff member could also be a user so no use re-loading staff variable if user is set
+            if (!empty($user)) 
+                $staff = Staff::getIdByEmail($mailinfo['email']);
+                
             //We have a valid ticket and user
             if (!empty($staff) || $ticket->getUserId() == $user->getId() //owner
                     ||  ($c = Collaborator::lookup( // check if collaborator
