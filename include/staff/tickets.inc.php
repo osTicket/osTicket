fd774abfd0f645a367803236682dfa3004ec0750
jdelhome3578 <jdelaney@tallmadge-ohio.org>
2015-06-09 14:46:52 -0400

    Update tickets.inc.php

--- include/staff/tickets.inc.php
+++ include/staff/tickets.inc.php
@@ -30,6 +30,11 @@ switch(strtolower($_REQUEST['status'])){ //Status is overloaded
         $status='open';
 		$results_type=__('Open Tickets');
         break;
+    case 'pending':
+        $status='pending';
+        $showpending=true;
+		$results_type=__('Pending Tickets');
+        break;
     case 'closed':
         $status='closed';
 		$results_type=__('Closed Tickets');
@@ -105,6 +110,8 @@ if($staffId && ($staffId==$thisstaff->getId())) { //My tickets
     $qwhere.=' AND ticket.isoverdue=1 ';
 }elseif($showanswered) { ////Answered
     $qwhere.=' AND ticket.isanswered=1 ';
+}elseif($showpending) { ////Pending
+    $qwhere.=' AND status.name="pending" ';
 }elseif(!strcasecmp($status, 'open') && !$search) { //Open queue (on search OPEN means all open tickets - regardless of state).
     //Showing answered tickets on open queue??
     if(!$cfg->showAnsweredTickets())
