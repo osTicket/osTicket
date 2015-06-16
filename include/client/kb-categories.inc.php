1129782b340457d8c67bb5027a99f17fe9734251
slm4996 <slm4996+git@gmail.com>
2015-06-16 08:34:38 -0400

    KB categories page now renders without a search query

--- include/client/kb-categories.inc.php
+++ include/client/kb-categories.inc.php
@@ -1,6 +1,6 @@
 <div class="row">
-  <div class="col-xs-12 col-sm-8">
-    <?php
+    <div class="col-xs-12 col-sm-8">
+        <?php
     $categories = Category::objects()
         ->exclude(Q::any(array(
             'ispublic'=>Category::VISIBILITY_PRIVATE,
@@ -14,23 +14,23 @@
             <div class="col-xs-12">
 <?php
         foreach ($categories as $C) { ?>
-            <h3><?php echo sprintf('<a href="faq.php?cid=%d">%s (%d)</a>',
-                $C->getId(), Format::htmlchars($C->getLocalName()); ?> </h3>
+            <h3><?php echo sprintf('<a href="faq.php?cid=%d">%s</a>',
+                $C->getId(), Format::htmlchars($C->getLocalName())); ?></h3>
             <div class="list-group">
-                <div class="listg-group-item text-muted">
+                <div class="list-group-item text-muted">
                     <?php echo Format::safe_html($C->getLocalDescriptionWithImages()); ?>
                     <span class="badge"><?php echo $C->faq_count; ?></span>
                 </div>
-<?php           foreach ($C->faqs
-                        ->exclude(array('ispublished'=>FAQ::VISIBILITY_PRIVATE))
-                        ->order_by('-views')->limit(5) as $F) { ?>
-                    <div class="list-group-item">
-                        <span class="glyphicon glyphicon-file" aria-hidden="true"></span>
-                        <a href="faq.php?id=<?php echo $F->getId(); ?>">
-                            <?php echo $F->getLocalQuestion() ?: $F->getQuestion(); ?>
-                        </a>
-                    </div>
-<?php           } ?>
+<?php       foreach ($C->faqs
+                    ->exclude(array('ispublished'=>FAQ::VISIBILITY_PRIVATE))
+                    ->order_by('-views')->limit(5) as $F) { ?>
+                <div class="list-group-item">
+                    <span class="glyphicon glyphicon-file" aria-hidden="true"></span>
+                    <a href="faq.php?id=<?php echo $F->getId(); ?>">
+                        <?php echo $F->getLocalQuestion() ?: $F->getQuestion(); ?>
+                    </a>
+                </div>
+<?php       } ?>
             </div>
 <?php   } ?>
             </div>
@@ -38,32 +38,33 @@
 <?php
     } else {
         echo __('NO FAQs found');
-      } ?>
-  </div>
-  <div class="col-xs-12 col-sm-4">
-    <div class="searchbar">
-        <form method="get" action="faq.php">
-        <input type="hidden" name="a" value="search"/>
-        <select class="form-control" name="topicId"
-            onchange="javascript:this.form.submit();">
-            <option value="">— Browse by Topic —</option>
-<?php
-$topics = Topic::objects()
-    ->annotate(array('has_faqs'=>SqlAggregate::COUNT('faqs')))
-    ->filter(array('has_faqs__gt'=>0));
-foreach ($topics as $T) { ?>
-        <option value="<?php echo $T->getId(); ?>"><?php echo $T->getFullName();
-            ?></option>
-<?php } ?>
-        </select>
-        </form>
+    }
+?>
     </div>
-    <br/>
-    <div class="content">
-        <div class="panel panel-primary">
-            <div class="panel-heading"><?php echo __('Other Resources'); ?></div>
-            <div class="panel-body"></div>
+    <div class="col-xs-12 col-sm-4">
+        <div class="searchbar">
+            <form method="get" action="faq.php">
+                <input type="hidden" name="a" value="search"/>
+                <select class="form-control" name="topicId"
+                    onchange="javascript:this.form.submit();">
+                    <option value="">— Browse by Topic —</option>
+<?php
+                    $topics = Topic::objects()
+                        ->annotate(array('has_faqs'=>SqlAggregate::COUNT('faqs')))
+                        ->filter(array('has_faqs__gt'=>0));
+                    foreach ($topics as $T) { ?>
+                        <option value="<?php echo $T->getId(); ?>"><?php echo $T->getFullName();?></option>
+<?php               } ?>
+                </select>
+            </form>
+        </div>
+        <br/>
+        <div class="content">
+            <div class="panel panel-primary">
+                <div class="panel-heading"><?php echo __('Other Resources'); ?></div>
+                <div class="panel-body"></div>
+            </div>
         </div>
     </div>
-  </div>
+</div>
 </div>
