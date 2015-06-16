8667ab0504abd8d07a29b155fd1cb79d3fa7d363
slm4996 <slm4996+git@gmail.com>
2015-06-16 09:06:56 -0400

    Fix client FAQ attachment listing

--- include/client/faq.inc.php
+++ include/client/faq.inc.php
@@ -29,7 +29,6 @@ $category=$faq->getCategory();
 <div class="col-xs-12 col-sm-4">
     <div class="searchbar">
         <form method="get" action="faq.php">
-        <div class="input-group">
             <div class="input-group">
                 <input type="hidden" name="a" value="search"/>
                 <input type="text" name="q" class="search form-control" placeholder="Search our knowledge base"/>
@@ -37,36 +36,33 @@ $category=$faq->getCategory();
                     <button type="submit" class="btn btn-success">Search</button>
                 </span>
             </div>
-        </div>
         </form>
     </div>
     <div class="clearfix">&nbsp;</div>
     <div class="content">
         <div class="panel panel-primary">
+            <div class="panel-heading"><?php echo __('Attachmments'); ?></div>
+            <div class="panel-body">
+                <?php if ($attachments = $faq->getLocalAttachments()->all()) {
+                    foreach ($attachments as $att) { ?>
+                        <a href="<?php echo $att->file->getDownloadUrl(); ?>" class="no-pjax">
+                            <i class="icon-file"></i>
+                            <?php echo Format::htmlchars($att->file->name); ?>
+                        </a>
+                    <?php } 
+                } ?>
+            </div>
+        </div>
+        <div class="panel panel-primary">
             <div class="panel-heading"><?php echo __('Help Topics'); ?></div>
             <div class="panel-body">
-                <?php if ($attachments = $faq->getLocalAttachments()->all()) { ?>
-                    <section>
-                    <strong><?php echo __('Attachments');?>:</strong>
-                    <?php foreach ($attachments as $att) { ?>
-                        <div>
-                            <a href="file.php?h=<?php echo $att['download']; ?>" class="no-pjax">
-                                <i class="icon-file"></i>
-                                <?php echo Format::htmlchars($att['name']); ?>
-                            </a>
-                        </div>
-                    <?php } ?>
-                    </section>
-                <?php }
-                if ($faq->getHelpTopics()->count()) { ?>
-                    <section>
-                        <strong><?php echo __('Help Topics'); ?></strong>
-                        <?php foreach ($faq->getHelpTopics() as $topic) { ?>
-                            <div><?php echo $topic->getFullName(); ?></div>
-                        <?php } ?>
-                    </section>
-                <?php }
-            ?></div>
+                <?php if ($faq->getHelpTopics()->count()) { ?>
+                    <strong><?php echo __('Help Topics'); ?></strong>
+                    <?php foreach ($faq->getHelpTopics() as $topic) { ?>
+                        <div><?php echo $topic->getFullName(); ?></div>
+                    <?php }
+                } ?>
+            </div>
         </div>
     </div>
 </div>
