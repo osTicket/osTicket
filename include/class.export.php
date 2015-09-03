b34ae348cc07d48dae21fe6e2e94ba0c5550a85f
Peter Rotich <peter@osticket.com>
2015-09-03 13:38:08 +0000

    orm: Prefer model relationship over local variable.
    The change helps avoid local variables from overwriting model joins.

--- include/class.export.php
+++ include/class.export.php
@@ -59,7 +59,7 @@ class Export {
         // Reset the $sql query
         $tickets = $sql->models()
             ->select_related('user', 'user__default_email', 'dept', 'staff',
-                'team', 'staff', 'cdata', 'topic', 'status', 'cdata.priority')
+                'team', 'staff', 'cdata', 'topic', 'status', 'cdata__priority')
             ->annotate(array(
                 'collab_count' => TicketThread::objects()
                     ->filter(array('ticket__ticket_id' => new SqlField('ticket_id', 1)))
