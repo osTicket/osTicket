fd16ecacdc40a0f36b3fbfe87e3d56adf8fb21cb
Jared Hancock <jared@osticket.com>
2015-09-21 12:46:55 -0500

    queue: Add install and migration scripts

---
+++ include/upgrader/streams/core/98ad7d55-00000000.patch.sql
@@ -0,0 +1,31 @@
+/**
+ * @version v1.11
+ * @signature 00000000000000000000000000000000
+ *
+ * Add custom queues, custom columns, and quick filter capabilities to the
+ * system.
+ */
+
+ALTER TABLE `%TABLE_PREFIX%queue`
+  ADD `filter` varchar(64) AFTER `config`,
+  ADD `root` varchar(32) DEFAULT NULL AFTER `filter`,
+  ADD `path` varchar(80) NOT NULL DEFAULT '/' AFTER `root`;
+
+DROP TABLE IF EXISTS `%TABLE_PREFIX%queue_column`;
+CREATE TABLE `%TABLE_PREFIX%queue_column` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `queue_id` int(10) unsigned NOT NULL,
+  `flags` int(10) unsigned NOT NULL DEFAULT '0',
+  `sort` int(10) unsigned NOT NULL DEFAULT '0',
+  `heading` varchar(64) NOT NULL DEFAULT '',
+  `primary` varchar(64) NOT NULL DEFAULT '',
+  `secondary` varchar(64) DEFAULT NULL,
+  `width` int(10) unsigned DEFAULT NULL,
+  `link` varchar(32) DEFAULT NULL,
+  `truncate` varchar(16) DEFAULT NULL,
+  `annotations` text,
+  `conditions` text,
+  `extra` text,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
--- setup/inc/streams/core/install-mysql.sql
+++ setup/inc/streams/core/install-mysql.sql
@@ -832,9 +832,30 @@ CREATE TABLE `%TABLE_PREFIX%queue` (
   `sort` int(11) unsigned not null default 0,
   `title` varchar(60),
   `config` text,
+  `filter` varchar(64),
+  `root` varchar(32) DEFAULT NULL,
+  `path` varchar(80) NOT NULL DEFAULT '/',
   `created` datetime not null,
   `updated` datetime not null,
   primary key (`id`)
+) DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `%TABLE_PREFIX%queue_column`;
+CREATE TABLE `%TABLE_PREFIX%queue_column` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `queue_id` int(10) unsigned NOT NULL,
+  `flags` int(10) unsigned NOT NULL DEFAULT '0',
+  `sort` int(10) unsigned NOT NULL DEFAULT '0',
+  `heading` varchar(64) NOT NULL DEFAULT '',
+  `primary` varchar(64) NOT NULL DEFAULT '',
+  `secondary` varchar(64) DEFAULT NULL,
+  `width` int(10) unsigned DEFAULT NULL,
+  `link` varchar(32) DEFAULT NULL,
+  `truncate` varchar(16) DEFAULT NULL,
+  `annotations` text,
+  `conditions` text,
+  `extra` text,
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `%TABLE_PREFIX%translation`;
