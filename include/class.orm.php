fd0262b256ad62eb6cafb1264a782153470b050d
Jared Hancock <jared@osticket.com>
2015-10-07 21:20:55 -0400

    queue: Implement manual sorting

--- include/class.orm.php
+++ include/class.orm.php
@@ -1868,7 +1868,7 @@ implements Iterator {
      * database.
      */
     function findAll(array $criteria, $limit=false) {
-        $records = array();
+        $records = new ListObject();
         foreach ($this as $record) {
             $matches = true;
             foreach ($criteria as $field => $check) {
