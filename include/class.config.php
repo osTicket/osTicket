012eb42b149a50e1b94f6e576d5448513509edb3
Jared Hancock <jared@osticket.com>
2015-10-08 11:58:49 -0400

    queue: Add default queue system setting
    This retires the "Show assigned tickets on open queue" and "Show answered
    tickets on open queue" settings.

--- include/class.config.php
+++ include/class.config.php
@@ -914,12 +914,15 @@ class OsticketConfig extends Config {
         return $this->get('auto_claim_tickets');
     }
 
+    // XXX: Drop these!!
     function showAssignedTickets() {
         return ($this->get('show_assigned_tickets'));
     }
-
     function showAnsweredTickets() {
         return ($this->get('show_answered_tickets'));
+    }
+    function getDefaultTicketQueueId() {
+        return $this->get('default_ticket_queue');
     }
 
     function hideStaffName() {
@@ -1238,6 +1241,12 @@ class OsticketConfig extends Config {
         if (!preg_match('`(?!<\\\)#`', $vars['ticket_number_format']))
             $errors['ticket_number_format'] = 'Ticket number format requires at least one hash character (#)';
 
+        require_once INCLUDE_DIR . 'class.queue.php';
+        if (!isset($vars['default_ticket_queue']))
+            $errors['default_ticket_queue'] = __("Select a default ticket queue");
+        elseif (!CustomQueue::lookup($vars['default_ticket_queue']))
+            $errors['default_ticket_queue'] = __("Select a default ticket queue");
+
         $this->updateAutoresponderSettings($vars, $errors);
         $this->updateAlertsSettings($vars, $errors);
 
@@ -1245,7 +1254,6 @@ class OsticketConfig extends Config {
             return false;
 
         // Sort ticket queues
-        require_once INCLUDE_DIR . 'class.queue.php';
         $queues = CustomQueue::objects()->all();
         foreach ($vars['qsort'] as $queue_id => $sort) {
             if ($q = $queues->findFirst(array('id' => $queue_id))) {
@@ -1264,11 +1272,10 @@ class OsticketConfig extends Config {
             'max_open_tickets'=>$vars['max_open_tickets'],
             'enable_captcha'=>isset($vars['enable_captcha'])?1:0,
             'auto_claim_tickets'=>isset($vars['auto_claim_tickets'])?1:0,
-            'show_assigned_tickets'=>isset($vars['show_assigned_tickets'])?0:1,
-            'show_answered_tickets'=>isset($vars['show_answered_tickets'])?0:1,
             'show_related_tickets'=>isset($vars['show_related_tickets'])?1:0,
             'allow_client_updates'=>isset($vars['allow_client_updates'])?1:0,
             'ticket_lock' => $vars['ticket_lock'],
+            'default_ticket_queue'=>$vars['default_ticket_queue'],
         ));
     }
 
