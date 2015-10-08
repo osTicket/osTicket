4c0a159bf927026e21f8485cc6abdeedb50688c4
Jared Hancock <jared@osticket.com>
2015-10-08 15:56:08 -0400

    queue: Integrate advanced search system

--- include/class.forms.php
+++ include/class.forms.php
@@ -3919,7 +3919,7 @@ class FreeTextWidget extends Widget {
             echo Format::viewableImages($config['content']); ?></div>
         </div>
         <?php
-        if (($attachments=$this->field->getFiles())) { ?>
+        if (($attachments = $this->field->getFiles()) && count($attachments)) { ?>
             <section class="freetext-files">
             <div class="title"><?php echo __('Related Resources'); ?></div>
             <?php foreach ($attachments as $attach) { ?>
