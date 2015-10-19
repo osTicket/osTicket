9cc23ec82fce386a022e82d96e380f84cb497601
Jared Hancock <jared@osticket.com>
2015-10-19 18:21:53 -0500

    queue: Add personal queue support
    This adds support for adding "personal" queues underneath any top-level
    queue with automatic criteria inheritance.

--- include/staff/templates/queue-navigation.tmpl.php
+++ include/staff/templates/queue-navigation.tmpl.php
@@ -4,30 +4,31 @@
 // $q - <CustomQueue> object for this navigation entry
 // $selected - <bool> true if this queue is currently active
 // $child_selected - <bool> true if the selected queue is a descendent
-$queue = $q;
-$selected = $_REQUEST['queue'] == $queue->getId();
+$this_queue = $q;
+$selected = $_REQUEST['queue'] == $this_queue->getId();
 ?>
 <li class="item <?php if ($child_selected) echo 'child active';
     elseif ($selected) echo 'active'; ?>">
-  <a href="<?php echo $queue->getHref(); ?>"><i class="icon-sort-down pull-right"></i><?php echo $queue->getName(); ?></a>
+  <a href="<?php echo $this_queue->getHref(); ?>"><i class="icon-sort-down pull-right"></i><?php echo $this_queue->getName(); ?></a>
   <div class="customQ-dropdown">
     <ul class="scroll-height">
       <!-- Start Dropdown and child queues -->
-      <?php foreach ($queue->getPublicChildren() as $q) {
+      <?php foreach ($this_queue->getPublicChildren() as $q) {
           include 'queue-subnavigation.tmpl.php';
       } ?>
       <!-- Dropdown Titles -->
       <li>
         <h4><?php echo __('Personal Queues'); ?></h4>
       </li>
-      <?php foreach ($queue->getMyChildren() as $q) {
+      <?php foreach ($this_queue->getMyChildren() as $q) {
         include 'queue-subnavigation.tmpl.php';
       } ?>
     </ul>
     <!-- Add Queue button sticky at the bottom -->
     <div class="add-queue">
       <a class="flush-right full-width" onclick="javascript:
-        $.dialog('ajax.php/tickets/search', 201);">
+        var pid = <?php echo $this_queue->getId() ?: 0; ?>;
+        $.dialog('ajax.php/tickets/search?parent_id='+pid, 201);">
         <div class="add pull-right"><i class="green icon-plus-sign"></i></div>
           <span><?php echo __('Add personal queue'); ?></span>
       </a>
