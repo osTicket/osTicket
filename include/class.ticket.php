ba85a10cbd40151072c308cc56b953478ead7eb8
Peter Rotich <peter@osticket.com>
2016-03-24 17:27:16 +0000

    i18n: Translate ticket sources

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -233,7 +233,13 @@ class Ticket {
     }
 
     static function getSources() {
-        return self::$sources;
+        static $translated = false;
+        if (!$translated) {
+            foreach (static::$sources as $k=>$v)
+                static::$sources[$k] = __($v);
+        }
+
+        return static::$sources;
     }
 
     //Getters
