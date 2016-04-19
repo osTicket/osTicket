0f0a6d3436b328d67550010134adde7ee77f2b98
Josh Richet <josh@joshrichet.com>
2016-04-19 12:35:54 -0700

    Update class.ticket.php

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -3328,6 +3328,12 @@ implements RestrictedAccess, Threadable {
 
         //We are ready son...hold on to the rails.
         $number = $topic ? $topic->getNewTicketNumber() : $cfg->getNewTicketNumber();
+        
+        //set the create date
+        if( isset($vars['date']) == FALSE ) {
+            $vars['date'] = date( 'Y-m-d G:i' );
+        }
+        
         $ticket = new static(array(
             'created' => date('Y-m-d G:i', strtotime( $vars['date'] ) ),
             'lastupdate' => date('Y-m-d G:i', strtotime( $vars['date'] ) ),
