14de707159927ca46b11c4732bc60a334adc3093
Jared Hancock <jared@osticket.com>
2016-04-24 17:43:02 -0500

    lint: Allow $this inside anonymous functions

--- setup/test/tests/class.php_analyze.php
+++ setup/test/tests/class.php_analyze.php
@@ -201,11 +201,10 @@ class SourceAnalyzer extends Test {
                 // PHP does not automatically nest scopes. Variables
                 // available inside the closure must be explictly defined.
                 // Therefore, there is no need to pass the current scope.
-                // However, $this is not allowed inside inline functions
-                // unless declared in the use () parameters.
+                // As of PHP 5.4, $this is added to the closure automatically
                 $this->traverseFunction(
                     array($token[2], $function['file']),
-                    array('allow_this'=>false));
+                    array('allow_this'=>true));
                 break;
             case T_STATIC:
                 $c = current($this->tokens);
