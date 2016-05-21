9ee5302b88995372da60674e437836e3597f5b25
Jared Hancock <jared@osticket.com>
2016-05-21 17:55:15 -0500

    queue: Implement enable/disable and delete

--- include/staff/queues-ticket.inc.php
+++ include/staff/queues-ticket.inc.php
@@ -8,19 +8,19 @@
             <div id="action-dropdown-more" class="action-dropdown anchor-right">
                 <ul id="actions">
                     <li>
-                        <a class="confirm" data-name="enable" href="queues.php?t=tickets&amp;a=enable">
+                        <a class="queue-action no-pjax" data-action="enable" href="#queues.php">
                             <i class="icon-ok-sign icon-fixed-width"></i>
                             <?php echo __( 'Enable'); ?>
                         </a>
                     </li>
                     <li>
-                        <a class="confirm" data-name="disable" href="queues.php?t=tickets&amp;a=disable">
+                        <a class="queue-action no-pjax" data-action="disable" href="#queues.php">
                             <i class="icon-ban-circle icon-fixed-width"></i>
                             <?php echo __( 'Disable'); ?>
                         </a>
                     </li>
                     <li class="danger">
-                        <a class="confirm" data-name="delete" href="queues.php?t=tickets&amp;a=delete#queues">
+                        <a class="queue-action no-pjax" data-action="delete" href="#queues.php">
                             <i class="icon-trash icon-fixed-width"></i>
                             <?php echo __( 'Delete'); ?>
                         </a>
@@ -28,12 +28,11 @@
                 </ul>
             </div>
         </div>
+        <input type="hidden" name="do" value="mass_process" />
         <h3><?php echo __('Ticket Queues');?></h3>
     </div>
     <div class="clear"></div>
  <?php csrf_token(); ?>
- <input type="hidden" name="do" value="mass_process" >
- <input type="hidden" id="action" name="a" value="" >
  <table class="list" border="0" cellspacing="1" cellpadding="0" width="940">
     <thead>
         <tr>
@@ -55,7 +54,7 @@ $emitLevel = function($queues, $level=0) use ($all_queues, &$emitLevel) {
         <td colspan="<?php echo max(1, $level); ?>"></td>
 <?php } ?>
         <td>
-          <input type="checkbox" class="checkbox" name="ckb[]">
+          <input type="checkbox" class="mass checkbox" value="<?php echo $q->id; ?>" />
           <input type="hidden" name="qsort[<?php echo $q->id; ?>]"
             value="<?php echo $q->sort; ?>"/>
         </td>
@@ -80,3 +79,36 @@ $emitLevel($all_queues->findAll(array('parent_id' => 0)));
 ?>
     </tbody>
 </table>
+
+<script>
+$(function() {
+  var goBaby = function(action) {
+    var ids = [], that = this,
+        $form = $(this).closest('form');
+    $('input:checkbox.mass:checked', $form).each(function() {
+      ids.push($(this).val());
+    });
+    if (ids.length) {
+      $.confirm(__('You sure?')).then(function() {
+        $.each(ids, function() { $form.append($('<input type="hidden" name="ids[]">').val(this)); });
+        $form.append($('<input type="hidden" name="a" />')
+          .val($(that).data('action')));
+        $form.append($('<input type="hidden" name="count" />')
+          .val(ids.length));
+        $form.attr('action', action);
+        // I don't know why, but submitting the form doesn't work...
+        $form.find('[type=submit]').trigger('click');
+      });
+    }
+    else {
+      $.sysAlert(__('Oops'),
+        __('You need to select at least one item'));
+    }
+  };
+  $(document).on('click', 'a.queue-action', function(e) {
+    e.preventDefault();
+    goBaby.call(this, $(this).attr('href').substr(1));
+    return false;
+  });
+});
+</script>
