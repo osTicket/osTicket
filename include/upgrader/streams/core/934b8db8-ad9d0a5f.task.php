6e10a2974db707e4672e49b9455c9c51010400a5
Jared Hancock <jared@osticket.com>
2016-05-21 21:29:55 -0500

    queue: Fix upgrade crash
    - %queue_sorts needs to exist before queues are inserted
    - old queue id numbers need to be moved up to make room for initial queues

--- include/upgrader/streams/core/934b8db8-ad9d0a5f.task.php
+++ include/upgrader/streams/core/934b8db8-ad9d0a5f.task.php
@@ -1,10 +1,22 @@
 <?php
 
 class QueueSortCreator extends MigrationTask {
-    var $description = "Load customziable sorting for ticket queues";
+    var $description = "Load customziable ticket queues";
 
     function run($time) {
         $i18n = new Internationalization('en_US');
+        $columns = $i18n->getTemplate('queue_column.yaml')->getData();
+        foreach ($columns as $C) {
+            QueueColumn::__create($C);
+        }
+        // Make room for the new queues starting at ID 1
+        $id = new SqlField('id');
+        CustomQueue::objects()->update(['id' => $id->plus(30)]);
+        $queues = $i18n->getTemplate('queue.yaml')->getData();
+        foreach ($queues as $C) {
+            CustomQueue::__create($C);
+        }
+
         $columns = $i18n->getTemplate('queue_sort.yaml')->getData();
         foreach ($columns as $C) {
             QueueSort::__create($C);
@@ -20,6 +32,10 @@ class QueueSortCreator extends MigrationTask {
             $q->flags |= CustomQueue::FLAG_INHERIT_SORTING;
             $q->save();
         }
+
+        // Set default queue to 'open'
+        global $cfg;
+        $cfg->set('default_ticket_queue', 1);
     }
 }
 return 'QueueSortCreator';
