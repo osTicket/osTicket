469f238d3fb32cfaa7af5396cc70b6238fa54f47
Kest874 <ken.stoner@nasg.net>
2016-06-17 08:48:07 -0500

    API Change allow based on api key only

--- include/class.api.php
+++ include/class.api.php
@@ -175,7 +175,7 @@ class ApiController {
 
         if(!($key=$this->getApiKey()))
             return $this->exerr(401, __('Valid API key required'));
-        elseif (!$key->isActive() || $key->getIPAddr()!=$_SERVER['REMOTE_ADDR'])
+        elseif (!$key->isActive() )//|| $key->getIPAddr()!=$_SERVER['REMOTE_ADDR'])
             return $this->exerr(401, __('API key not found/active or source IP not authorized'));
 
         return $key;
@@ -184,7 +184,7 @@ class ApiController {
     function getApiKey() {
 
         if (!$this->apikey && isset($_SERVER['HTTP_X_API_KEY']) && isset($_SERVER['REMOTE_ADDR']))
-            $this->apikey = API::lookupByKey($_SERVER['HTTP_X_API_KEY'], $_SERVER['REMOTE_ADDR']);
+            $this->apikey = API::lookupByKey($_SERVER['HTTP_X_API_KEY']);//, $_SERVER['REMOTE_ADDR']);
 
         return $this->apikey;
     }
