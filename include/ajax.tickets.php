2be652646881290a5f3fae6adcd8ac8467bdc20c
Jared Hancock <jared@osticket.com>
2016-06-25 09:37:11 -0500

    orm: Fix warning for read-only permission
    This fixes an issue where the `$ht` variable is not properly initialized by
    the ORM. It is assumed to be a valid array in the constructor, and its items
    are run through the ::set() method. Therefore, if an empty array is
    received, the `$ht` variable will remain null, since nothing is ever set.
    
    This also fixes a misspelled variable in the claim dialog if a ticket is
    currently assigned to a team only.

--- include/ajax.tickets.php
+++ include/ajax.tickets.php
@@ -502,7 +502,7 @@ class TicketsAjaxAPI extends AjaxController {
             if ($ticket->getStaffId() == $thisstaff->getId())
                 $assigned = __('you');
             else
-                $assigneed = $ticket->getAssigned();
+                $assigned = $ticket->getAssigned();
 
             $info['error'] = sprintf(__('%s is currently assigned to <b>%s</b>'),
                     __('This ticket'),
