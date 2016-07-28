5a84f54b445fa5eece262d8f471797d042335537
Arthur Borsboom <arthurborsboom@gmail.com>
2016-07-28 16:11:00 +0200

    Ticket by email: reduce chances of incorrectly creating a new ticket.
    One of the methods used by osTicket to find a related ticket number in an
    email is by looking in the subject of the email for a ticket number, such
    as [#123456]. If for some reason the subject contains two reference
    numbers, such as the osTicket reference number (ie. [#543521]) and a
    foreign reference number (ie. #654321), then osTicket might pickup the wrong
    signature. This might result in the creation of a new ticket, while the
    contents of the email should have been added to an existing ticket.
    An example subject leading to this issue:
    
    Company X #12345 Need help installing software [#987654]
    
    Since the square opening bracket is always in the signature of the osTicket
    subject anyway, this can be used to better pattern recognition. By making
    this square opening bracket a requirement for ticket signature recognition,
    it reduces the changes of picking up the wrong signature.
    
    This commit adds the requirement for the square opening bracket of the
    signature in the email subject.

--- include/class.thread.php
+++ include/class.thread.php
@@ -1223,7 +1223,7 @@ implements TemplateVariable {
                 // Required `#` followed by one or more of
                 //      punctuation (-) then letters, numbers, and symbols
                 // (Try not to match closing punctuation (`]`) in [#12345])
-                && preg_match("/#((\p{P}*[^\p{C}\p{Z}\p{P}]+)+)/u", $subject, $match)
+                && preg_match("/\[#((\p{P}*[^\p{C}\p{Z}\p{P}]+)+)/u", $subject, $match)
                 //Lookup by ticket number
                 && ($ticket = Ticket::lookupByNumber($match[1]))
                 //Lookup the user using the email address
