1694f44e56992a45be7608e8d860d7fb725e8753
martin-rueegg <martin.rueegg@metaworx.ch>
2016-10-04 21:43:40 +0100

    x autoload position

--- bootstrap.php
+++ bootstrap.php
@@ -307,8 +307,6 @@ $here = ($h = realpath($here)) ? $h : $here;
 define('ROOT_DIR',str_replace('\\', '/', $here.'/'));
 unset($here); unset($h);
 
-require ROOT_DIR . '/vendor/autoload.php';
-
 define('INCLUDE_DIR',ROOT_DIR.'include/'); //Change this if include is moved outside the web path.
 define('SETUP_DIR',ROOT_DIR.'setup/');
 
@@ -332,6 +330,8 @@ if(!defined('PATH_SEPARATOR')){
 
 //Set include paths. Overwrite the default paths.
 ini_set('include_path', './'.PATH_SEPARATOR.INCLUDE_DIR);
+
+require ROOT_DIR . '/vendor/autoload.php';
 
 require(INCLUDE_DIR.'class.osticket.php');
 require(INCLUDE_DIR.'class.misc.php');
