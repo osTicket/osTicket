f88f14f1b5e04205b642944364c8251a1ef5590c
jdelhome3578 <jdelaney@tallmadge-ohio.org>
2016-10-20 12:53:42 -0400

    Cleanup
    Remove code not used

--- include/client/tickets.inc.php
+++ include/client/tickets.inc.php
@@ -47,9 +47,8 @@ if($sort && $sortOptions[$sort])
     $order_by =$sortOptions[$sort];
 
 $order_by=$order_by ?: $sortOptions['date'];
-$order=$_REQUEST['order']?$orderWays[strtoupper($_REQUEST['order'])]:$orderWays['DESC'];
-if($_REQUEST['order'] && $orderWays[strtoupper($_REQUEST['order'])])
-    $order=$orderWays[strtoupper($_REQUEST['order'])];
+
+$order=strtoupper($_REQUEST['order'])?$orderWays[strtoupper($_REQUEST['order'])]:$orderWays['DESC'];
 
 $x=$sort.'_sort';
 $$x=' class="'.strtolower($_REQUEST['order'] ?: 'desc').'" ';
