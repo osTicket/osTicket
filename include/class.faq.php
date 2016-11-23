79586640c6cbd8d9b7c06641df9f5590b3e8e890
Paolo Bernardi <bernarpa@gmail.com>
2016-11-23 12:25:40 +0100

    Don't show featured FAQs when Knowledgebase access is restricted

--- include/class.faq.php
+++ include/class.faq.php
@@ -343,7 +343,7 @@ class FAQ extends VerySimpleModel {
 
     static function getFeatured() {
         return self::objects()
-            ->filter(array('ispublished__in'=>array(1,2), 'category__ispublic'=>1))
+            ->filter(array('ispublished__in'=>array(2), 'category__ispublic'=>1))
             ->order_by('-ispublished');
     }
 
