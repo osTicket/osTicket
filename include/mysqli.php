ba4d4a165bc8901ee983b3491b67d750984611ee
Ray Foss <ray@enhancesoft.com>
2017-01-03 15:19:02 -0600

    self ignore

--- include/mysqli.php
+++ include/mysqli.php
@@ -35,6 +35,8 @@ function db_connect($host, $user, $passwd, $options = array()) {
                 $options['ssl']['cert'],
                 $options['ssl']['ca'],
                 null, null);
+    elseif(!$passwd && OST_IS_DEVELOPER_MODE !== true)
+        return NULL;
 
     $port = ini_get("mysqli.default_port");
     $socket = ini_get("mysqli.default_socket");
