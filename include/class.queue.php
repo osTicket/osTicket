26c730f81a4c91b06f54a73a402785bb57e59ba2
Jared Hancock <jared@osticket.com>
2017-01-07 19:55:17 -0600

    queue: Fix inherit columns and queue condition
    If a queue inherits columns from a parent, the conditions would also be
    inherited. This fix changes the association by linking the inherited
    column to the child queue at run-time. This allows the queue column to
    fetch the queue conditions from the child queue which inherited the
    columns.

--- include/class.queue.php
+++ include/class.queue.php
@@ -513,7 +513,10 @@ class CustomQueue extends VerySimpleModel {
             && $this->hasFlag(self::FLAG_INHERIT_COLUMNS)
             && $this->parent
         ) {
-            return $this->parent->getColumns();
+            $columns = $this->parent->getColumns();
+            foreach ($columns as $c)
+                $c->setQueue($this);
+            return $columns;
         }
         elseif (count($this->columns)) {
             return $this->columns;
@@ -1635,6 +1638,7 @@ extends VerySimpleModel {
 
     var $_annotations;
     var $_conditions;
+    var $_queue;            // Apparent queue if being inherited
 
     function getId() {
         return $this->id;
@@ -1653,7 +1657,15 @@ extends VerySimpleModel {
     // These getters fetch data from the annotated overlay from the
     // queue_column table
     function getQueue() {
-        return $this->queue;
+        return $this->_queue ?: $this->queue;
+    }
+    /**
+     * If a column is inherited into a child queue and there are conditions
+     * added to that queue, then the column will need to be linked at
+     * run-time to the child queue rather than the parent.
+     */
+    function setQueue(CustomQueue $queue) {
+        $this->_queue = $queue;
     }
 
     function getWidth() {
