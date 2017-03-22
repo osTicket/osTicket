f8b9ca124372208b3cab805316a26b9a48b17b56
Anthony Kuske <anthonykuske@gmail.com>
2017-03-22 01:39:44 +0000

    Service & Response times on dashboard also honor selected timeframe

--- include/class.report.php
+++ include/class.report.php
@@ -144,6 +144,9 @@ class OverviewReport {
 
         list($start, $stop) = $this->getDateRange();
         $times = Ticket::objects()
+            ->filter(array(
+                'thread__created__range' => array($start, $stop),
+            ))
             ->constrain(array(
                 'thread__entries' => array(
                     'thread__entries__type' => 'R'
