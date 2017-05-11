c79706d8c0c1cf1046405f40c6b9db3fb069a646
Kest874 <ken.stoner@nasg.net>
2017-05-11 08:38:21 -0500

    Opps forgot files.

---
+++ include/staff/reports.inc.php
@@ -0,0 +1,80 @@
+<?php
+if(!defined('OSTSTAFFINC') || !$thisstaff) die('Access Denied');
+
+?>
+
+    <div class="has_bottom_border" style="margin-bottom:5px; padding-top:5px;">
+        <div class="pull-left">
+            <h2><?php echo __('Reports');?></h2>
+        </div>
+        <div class="clear"></div>
+    </div>
+<div>
+<div>
+<br>
+
+
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border:none;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:12px 10px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:12px 10px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg .tg-3we0{background-color:#ffffff;vertical-align:top}
+</style>
+
+
+<table class="tg" width="100%">
+
+    <tr class="tg-3we0">
+        <td width="220px">
+            <strong>
+                <a href="http://crpsql01/ReportServer/Pages/ReportViewer.aspx?%2fosTicket%2fIT%2fIT+Performance" target="_blank">
+                IT Performance</a>
+            </strong>
+        </td>
+        <td>
+            <em>Report showing IT performance.</em>
+        </td>
+    </tr>
+    <tr class="tg-3we0">
+        <td width="220px">
+            <strong>
+                <a href="http://crpsql01/ReportServer/Pages/ReportViewer.aspx?%2fosTicket%2fIT%2fOpenTicketsAge" target="_blank">
+                Open Ticket Age</a>
+            </strong>
+        </td>
+        <td>
+            <em>Report showing Open Ticket by age.</em>
+        </td>
+    </tr>
+    <tr class="tg-3we0">
+        <td width="220px">
+            <strong>
+                <a href="http://crpsql01/ReportServer/Pages/ReportViewer.aspx?%2fosTicket%2fIT%2fOpen+Issues+and+Suggestions" target="_blank">
+                Open Issues and Suggesitons</a>
+            </strong>
+        </td>
+        <td>
+            <em>Report showing Open Issues and Suggestions.</em>
+        </td>
+    </tr>
+        <tr class="tg-3we0">
+        <td width="220px">
+            <strong>
+                <a href="http://crpsql01/ReportServer/Pages/ReportViewer.aspx?%2fosTicket%2fIT%2fManagement+Review+Data&rs:Format=Excel" target="_blank">
+                Management Review Data</a>
+            </strong>
+        </td>
+        <td>
+            <em>Export Management Review Data (Excel).</em>
+        </td>
+    </tr>
+    
+ 
+</table>
+<br><br><br>
+<hr>
+<div>
+</div>
+
+
---
+++ scp/reports.php
@@ -0,0 +1,26 @@
+<?php
+/*********************************************************************
+    kb.php
+
+    Knowlegebase
+
+    Peter Rotich <peter@osticket.com>
+    Copyright (c)  2006-2013 osTicket
+    http://www.osticket.com
+
+    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+    See LICENSE.TXT for details.
+
+    vim: expandtab sw=4 ts=4 sts=4:
+**********************************************************************/
+require('staff.inc.php');
+require_once(INCLUDE_DIR.'class.faq.php');
+
+
+$inc='reports.inc.php'; //KB landing page.
+$nav->setTabActive('reports');
+
+require_once(STAFFINC_DIR.'header.inc.php');
+require_once(STAFFINC_DIR.$inc);
+require_once(STAFFINC_DIR.'footer.inc.php');
+?>
