bed575be3a87476338550bb45bae22d9422a1643
Diego Melo <diego_melo@unit.br>
2017-06-16 17:07:19 -0300

    Correção do link no footer

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -5,7 +5,7 @@
     <div class="container">
         <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - All rights reserved.</p>
         <a id="poweredBy" href="http://osticket.com" target="_blank">
-            <img src="/assets/default/images/poweredby.png" alt="<?php echo __('Helpdesk software - powered by osTicket'); ?>">
+            <img src="<?php echo ROOT_PATH; ?>/assets/default/images/poweredby.png" alt="<?php echo __('Helpdesk software - powered by osTicket'); ?>">
         </a>
     </div>
 </div>
@@ -20,7 +20,7 @@ if (($lang = Internationalization::getCurrentLanguage()) && $lang != 'en_US') { 
     <script type="text/javascript" src="ajax.php/i18n/<?php
         echo $lang; ?>/js"></script>
 <?php } ?>
-<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo ROOT_PATH; ?>/js/bootstrap.min.js"></script>
 <script type="text/javascript">
     getConfig().resolve(<?php
         include INCLUDE_DIR . 'ajax.config.php';
