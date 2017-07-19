e118db2480729ba592d076f95c0cc023505ffa2f
JediKev <kevin@enhancesoft.com>
2017-07-19 16:48:01 -0500

    cron: Delete Expired Sessions
    This addresses an issue where expired sessions would not be removed from
    the database. This caused the session table to fill up and create
    unnecessary issues. This adds a cleanup method to remove all expired
    sessions from the database.

--- include/class.cron.php
+++ include/class.cron.php
@@ -51,6 +51,11 @@ class Cron {
         AttachmentFile::deleteOrphans();
     }
 
+    function CleanExpiredSessions() {
+        require_once(INCLUDE_DIR.'class.ostsession.php');
+        DbSessionBackend::cleanup();
+    }
+
     function MaybeOptimizeTables() {
         // Once a week on a 5-minute cron
         $chance = rand(1,2000);
@@ -100,6 +105,7 @@ class Cron {
         self::MailFetcher();
         self::TicketMonitor();
         self::PurgeLogs();
+        self::CleanExpiredSessions();
         // Run file purging about every 10 cron runs
         if (mt_rand(1, 9) == 4)
             self::CleanOrphanedFiles();
