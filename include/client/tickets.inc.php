58735710613a48a38cc7842eb354f78aa28fe753
jdelhome3578 <jdelhome3578@users.noreply.github.com>
2017-10-27 10:52:59 -0400

    oops: Fix Client Ticket Sorting

--- include/client/tickets.inc.php
+++ include/client/tickets.inc.php
@@ -47,7 +47,7 @@ if($sort && $sortOptions[$sort])
     $order_by =$sortOptions[$sort];
 
 $order_by=$order_by ?: $sortOptions['date'];
-if ($_REQUEST['order'] && $orderWays[strtoupper($_REQUEST['order'])])
+if ($_REQUEST['order'])
     $order = $orderWays[strtoupper($_REQUEST['order'])];
 else
     $order = $orderWays['DESC'];
