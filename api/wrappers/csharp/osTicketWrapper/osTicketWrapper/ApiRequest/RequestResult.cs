65c1fce4ae774d72203a6d85ab5b5c0e93d1ce97
Pedro Luis <pedro-1451@hotmail.com>
2017-11-27 08:33:47 -0200

    Add osTicketWrapper

---
+++ api/wrappers/csharp/osTicketWrapper/osTicketWrapper/ApiRequest/RequestResult.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace osTicketWrapper.ApiRequest
+{
+    public class RequestResult
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public RestRequest Request { get; set; }
+        public string Function { get; set; }
+        public string Content { get; set; }
+        public Method Method { get; set; }
+        public string URL { get; set; }
+    }
+}
