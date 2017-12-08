2547f2dc4f955858c3bc9d6cb16d128b99e10f70
JediKev <kevin@enhancesoft.com>
2017-12-08 14:05:17 -0600

    issue: Fix Most Redactor Issues
    This addresses most Redactor issues we’ve been having lately (ie. Image
    Paste, Bold button doesn’t work, etc.). This updates Redactor from 10.2.2
    to 10.2.5 which seems to have resolved all the issues in testing. This
    should not affect the original functionality but if you notice any strange
    behaviors please let us know.

--- scp/js/scp.js
+++ scp/js/scp.js
@@ -214,6 +214,7 @@ var scp_prep = function() {
                     var box = $('#response',fObj),
                         redactor = box.data('redactor');
                     if(canned.response) {
+                        redactor.selection.restore();
                         if (redactor)
                             redactor.insert.html(canned.response);
                         else
