ab267a9a905914812d4b23e8f7b766d68e459494
Pieter Lenaerts <lenaerpi@acmidm-zandbak-pr-1.vm.cumuli.be>
2018-03-20 19:15:12 +0100

    Fix version, title, signature tags in upgrade sql

--- include/upgrader/streams/core/98ad7d55-9f415d0f.patch.sql
+++ include/upgrader/streams/core/98ad7d55-9f415d0f.patch.sql
@@ -1,10 +1,9 @@
-/* osTicket database migration patch
- *
+/**
  * @version v1.10.0+
  * @signature 9f415d0f424fdf609c76a8ad4fffefae
  * @title Increase staff username field to 64
  *
- * increases the ost_staff table username field length to 64
+ * Increases the ost_staff table username field length to 64
  */
 
 ALTER TABLE `%TABLE_PREFIX%staff` MODIFY `username` VARCHAR(64);
