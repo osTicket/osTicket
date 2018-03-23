fc5326549e1d1bf7507b6a82329e2e285654b7ed
aydreeihn <adriane@enhancesoft.com>
2018-03-23 13:10:20 -0500

    correctly filter departmentfield. use departmentfield for referrals

--- include/class.forms.php
+++ include/class.forms.php
@@ -2656,15 +2656,12 @@ class DepartmentField extends ChoiceField {
           else {
             $current_id = $selected->value;
             $current_name = Dept::getNameById($current_id);
-            $addNew = true;
           }
         }
 
-        $active_depts = array();
-        if($current_id)
-          $active_depts = Dept::objects()
-            ->filter(array('flags__hasbit' => Dept::FLAG_ACTIVE))
-            ->values('id', 'name');
+        $active_depts = Dept::objects()
+          ->filter(array('flags__hasbit' => Dept::FLAG_ACTIVE))
+          ->values('id', 'name');
 
         $choices = array();
         if ($depts = Dept::getDepartments(null, true, Dept::DISPLAY_DISABLED)) {
@@ -2674,18 +2671,17 @@ class DepartmentField extends ChoiceField {
             $active[$dept['id']] = $dept['name'];
 
           //add selected dept to list
-          $active[$current_id] = $current_name;
-
+          if($current_id)
+            $active[$current_id] = $current_name;
+          else
+            return $active;
 
           foreach ($depts as $id => $name) {
             $choices[$id] = $name;
             if(!array_key_exists($id, $active) && $current_id)
-              unset($choices[$id]);
+                unset($choices[$id]);
           }
-
         }
-        if($addNew)
-          $choices[':new:'] = '— '.__('Add New').' —';
 
         return $choices;
     }
@@ -3899,6 +3895,8 @@ class ChoicesWidget extends Widget {
                         $values[$v] = $choices[$v];
                     elseif (($i=$this->field->lookupChoice($v)))
                         $values += $i;
+                    elseif (!$k && $v)
+                      return $v;
                 }
             }
         }
@@ -4882,7 +4880,7 @@ class ReferralForm extends Form {
                               ),
                             )
                 ),
-            'dept' => new ChoiceField(array(
+            'dept' => new DepartmentField(array(
                     'id'=>4,
                     'label' => '',
                     'flags' => hexdec(0X450F3),
