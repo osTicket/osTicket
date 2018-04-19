e9563c567285caea5410875abe4c2d5cf036ea63
Jared Hancock <gravydish@gmail.com>
2018-04-18 20:12:37 -0500

    queryset: Fix circular reference error
    This fixes an error where the ModelInstanceManager maintained a reference to
    the QuerySet instance, and the QuerySet instance managed a reference to the
    ModelInstanceManager instance (if it's the iterator for the query). Because
    of the circular reference, if the iterator is not exhausted, then the
    resource is not closed and the query remains open. This wastes memory and
    prevents some other queries from running after such a situation happens.
    
    This addresses the issue by removing the circular reference between the
    QuerySet and the ModelInstanceManager.

--- include/class.orm.php
+++ include/class.orm.php
@@ -1801,15 +1801,22 @@ extends CachedResultSet {
 
 class ModelInstanceManager
 implements IteratorAggregate {
-    var $queryset;
     var $model;
     var $map;
+    var $resource;
+    var $annnotations;
+    var $defer;
 
     static $objectCache = array();
 
     function __construct(QuerySet $queryset) {
-        $this->queryset = $queryset;
         $this->model = $queryset->model;
+        $this->resource = $queryset->getQuery();
+        $cache = !$queryset->hasOption(QuerySet::OPT_NOCACHE);
+        $this->resource->setBuffered($cache);
+        $this->map = $this->resource->getMap();
+        $this->annotations = $queryset->annotations;
+        $this->defer = $queryset->defer;
     }
 
     function cache($model) {
@@ -1864,7 +1871,7 @@ implements IteratorAggregate {
                 return null;
             }
         }
-        $annotations = $this->queryset->annotations;
+        $annotations = $this->annotations;
         $extras = array();
         // For annotations, drop them from the $fields list and add them to
         // an $extras list. The fields passed to the root model should only
@@ -1883,7 +1890,7 @@ implements IteratorAggregate {
             // Construct and cache the object
             $m = $modelClass::__hydrate($fields);
             // XXX: defer may refer to fields not in this model
-            $m->__deferred__ = $this->queryset->defer;
+            $m->__deferred__ = $this->defer;
             $m->__onload();
             if ($cache)
                 $this->cache($m);
@@ -1953,10 +1960,6 @@ implements IteratorAggregate {
     }
 
     function getIterator() {
-        $this->resource = $this->queryset->getQuery();
-        $this->map = $this->resource->getMap();
-        $cache = !$this->queryset->hasOption(QuerySet::OPT_NOCACHE);
-        $this->resource->setBuffered($cache);
         $func = ($this->map) ? 'getRow' : 'getArray';
         $func = array($this->resource, $func);
 
