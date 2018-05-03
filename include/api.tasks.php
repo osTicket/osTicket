7c32cac149777820214998991dfb38910d401ca5
Mustafa Kerim Yılmaz <mustafa.yilmaz@isisbilisim.com.tr>
2018-05-03 15:14:34 +0300

    dynamic where for task api

--- include/api.tasks.php
+++ include/api.tasks.php
@@ -11,14 +11,41 @@ class TaskApiController extends ApiController {
         if(!($key=$this->requireApiKey()))
             return $this->exerr(401, __('API key not authorized'));
 
-        # Parse request body
-        $tasks = $this->reportTasks();
-        Http::response(200,  JsonDataEncoder::encode($tasks), 'application/json');
+
+        # Parse request query string
+        $filter = array();
+        parse_str($_SERVER['QUERY_STRING'], $filter);
+        foreach ($filter as $key => $value) {
+            $filter[$key . "__exact"] = $value;
+            unset($filter[$key]);
+        }
+
+        try {
+            $tasks = $this->reportTasks($filter);
+        } catch (InconsistentModelException $e) {
+            Http::response(400, $e->getMessage());
+            exit;
+        }
+        $encoder = null;
+        $contentType = null;
+        switch(strtolower($format)) {
+            case 'json':
+                $encoder = new JsonDataEncoder();
+                $contentType = 'application/json';
+                break;
+            default:
+                $this->exerr(415, __('Unsupported response format'));
+        }
+
+        Http::response(200,  $encoder->encode($tasks), $contentType);
         exit;
     }
 
-    function reportTasks() {
-        return Task::objects()->filter(array('flags__exact' => Task::ISOPEN))->all();
+    function reportTasks($filter) {
+        if (count($filter) == 0)
+            return TaskModel::objects()->all();
+        else
+            return TaskModel::objects()->filter($filter)->all();
     }
 
 }
