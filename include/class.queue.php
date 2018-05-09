d43ce3dafe5f2650ec6dd400234eac759942cb70
Peter Rotich <peter@enhancesoft.com>
2018-05-09 16:51:21 +0000

    COALESCE: Force nulls to the bottom

--- include/class.queue.php
+++ include/class.queue.php
@@ -2094,6 +2094,8 @@ extends VerySimpleModel {
                 list($path, $field) = $key;
                 $fields[] = new SqlField($path);
             }
+            // Force nulls to the buttom.
+            $fields[] = 'zzz';
 
             $alias = sprintf('C%d', $this->getId());
             $expr = call_user_func_array(array('SqlFunction', 'COALESCE'),
