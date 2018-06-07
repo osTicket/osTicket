e4329513bba0ba066b6e8ee4f6cd7c717e7c02e7
JediKev <kevin@enhancesoft.com>
2018-06-07 14:50:09 -0500

    issue: Auto-Assignment Log
    This addresses an issue on the Forums where the Auto-Assignment Thread
    Event (configurable via Help Topic) uses the Email of the User rather than
    the User’s Name. This adds the correct function to retrieve the User’s
    Name if the User has an Account.

--- include/class.thread.php
+++ include/class.thread.php
@@ -1791,8 +1791,8 @@ class ThreadEvents extends InstrumentedList {
             }
             // XXX: Use $user here
             elseif ($thisclient) {
-                if ($thisclient->hasAccount)
-                    $username = $thisclient->getAccount()->getUserName();
+                if ($thisclient->hasAccount())
+                    $username = $thisclient->getFullName();
                 if (!$username)
                     $username = $thisclient->getEmail();
             }
