e078e27d5d69ecd1b93c27bbf65ba57800302ca2
JediKev <kevin@enhancesoft.com>
2018-07-06 09:31:52 -0500

    issue: User Import No Email
    This addresses issue 4329 where you can import a User with no email
    address via CSV file. Once the User is added and you try to update them
    with an email address it fails due to no default email. This updates the
    check for email address from `!isset()` to `empty()` which will correctly
    check for empty string.

--- include/class.user.php
+++ include/class.user.php
@@ -507,7 +507,7 @@ implements TemplateVariable, Searchable {
             db_autocommit(false);
             $records = $importer->importCsv(UserForm::getUserForm()->getFields(), $defaults);
             foreach ($records as $data) {
-                if (!isset($data['email']) || !isset($data['name']))
+                if (!Validator::is_email($data['email']) || empty($data['name']))
                     throw new ImportError('Both `name` and `email` fields are required');
                 if (!($user = static::fromVars($data, true, true)))
                     throw new ImportError(sprintf(__('Unable to import user: %s'),
