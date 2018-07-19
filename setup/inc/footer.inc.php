f1f9fac0276d79deb67c42b3fa2637587b68eaa2
JediKev <kevin@enhancesoft.com>
2018-07-19 15:52:48 -0500

    issue: Installer Footer Copyright
    This addresses issue 4339 where the installer footer references the wrong
    copyright date.

--- setup/inc/footer.inc.php
+++ setup/inc/footer.inc.php
@@ -2,7 +2,7 @@
             <div class="clear"></div>
         </div> <!-- content -->
     </div> <!-- wizard -->
-    <div id="footer" class="centered">Copyright &copy; 2013 <a target="_blank" href="http://osticket.com">osTicket.com</a></div>
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="http://osticket.com">osTicket.com</a></div>
 
     <script type="text/javascript" src="../js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="../js/jstz.min.js"></script>
