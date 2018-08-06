osTicket/osTicket

commit 510046c51568f336556bce9d379f6d80630f8efb

Fixes found while writing test cases:
- function isReopenable needed to check for topic
- getHelpTopics for inline edit needed to include name of disabled Topic if selected
- getHelpTopics for dashboard needed to include disabled/archived Topics

include/class.ticket.php
@@ -207,9 +207,8 @@ implements RestrictedAccess, Threadable, Searchable {
     }
 
     function isReopenable() {
-        return ($this->getStatus()->isReopenable()
-          && $this->getDept()->allowsReopen()
-        && $this->getTopic()->allowsReopen());
+        return ($this->getStatus()->isReopenable() && $this->getDept()->allowsReopen()
+        && ($this->getTopic() ? $this->getTopic()->allowsReopen() : null));
     }
 
     function isClosed() {
@@ -968,7 +967,7 @@ implements RestrictedAccess, Threadable, Searchable {
                         'name' => "{$fid}_id",
                         'label' => __('Help Topic'),
                         'default' => $this->getTopicId(),
-                        'choices' => Topic::getHelpTopics()
+                        'choices' => Topic::getHelpTopics(false, Topic::DISPLAY_DISABLED)
                         ));
             break;
         case 'source':

include/class.report.php
@@ -211,7 +211,7 @@ class OverviewReport {
             $headers = array(__('Help Topic'));
             $header = function($row) { return Topic::getLocalNameById($row['topic_id'], $row['topic__topic']); };
             $pk = 'topic_id';
-            $topics = Topic::getHelpTopics();
+            $topics = Topic::getHelpTopics(false, Topic::DISPLAY_DISABLED);
             $stats = $stats
                 ->values('topic_id', 'topic__topic', 'topic__flags')
                 ->filter(array('dept_id__in' => $thisstaff->getDepts(), 'topic_id__gt' => 0, 'topic_id__in' => array_keys($topics)));

commit 310017b18fc0857c22131eaae8a0b73bbe8cf1f8

status: Allow Reopen
This addresses an issue introduced with `510046c5` where tickets with no
Help Topic can not be reopened. This is due to a check on `isReopenable()`
where it returns `null` instead of `true` if there is no help topic;
therefore returning false and not allowing the ticket to be reopened.

include/class.ticket.php
@@ -221,7 +221,7 @@ implements RestrictedAccess, Threadable, Searchable {
 
     function isReopenable() {
         return ($this->getStatus()->isReopenable() && $this->getDept()->allowsReopen()
-        && ($this->getTopic() ? $this->getTopic()->allowsReopen() : null));
+        && ($this->getTopic() ? $this->getTopic()->allowsReopen() : true));
     }
 
     function isClosed() {