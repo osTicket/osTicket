184898324e64559d67163542182fd63063de8daa
Jared Hancock <gravydish@gmail.com>
2018-08-14 03:51:32 +0000

    queues: cache queue counts
    If APCu is available, then the queue counts can be cached between requests.
    They are automatically cleared and recalculated if the status of a ticket
    changes or if a queue or saved search is edited. Otherwise, the queue counts
    will expire after an hour and be recalculated anyway.

--- include/class.queue.php
+++ include/class.queue.php
@@ -1249,8 +1249,11 @@ class CustomQueue extends VerySimpleModel {
         $nopath = !isset($this->path);
         $path_changed = isset($this->dirty['parent_id']);
 
-        if ($this->dirty)
+        if ($this->dirty) {
             $this->updated = SqlFunction::NOW();
+            // Refetch the queue counts
+            SavedQueue::clearCounts();
+        }
         if (!($rv = parent::save($refetch || $this->dirty)))
             return $rv;
 
