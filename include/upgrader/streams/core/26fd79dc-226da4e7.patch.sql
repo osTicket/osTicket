ec740de3922184112dd5e1f226afed0ca48e2051
aydreeihn <adriane@enhancesoft.com>
2018-09-07 09:53:14 -0500

    Update Thread Events in Batches
    Avoid php timeouts when upgrading databases with large amounts of data by updating Thread Events in batches.

--- include/upgrader/streams/core/26fd79dc-226da4e7.patch.sql
+++ include/upgrader/streams/core/26fd79dc-226da4e7.patch.sql
@@ -18,6 +18,23 @@ CREATE TABLE `%TABLE_PREFIX%event` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+INSERT INTO `%TABLE_PREFIX%event` (`id`, `name`, `description`)
+VALUES
+	(1,'created',''),
+	(2,'closed',''),
+	(3,'reopened',''),
+	(4,'assigned',''),
+	(5,'released',''),
+	(6,'transferred',''),
+	(7,'referred',''),
+	(8,'overdue',''),
+	(9,'edited',''),
+	(10,'viewed',''),
+	(11,'error',''),
+	(12,'collab',''),
+	(13,'resent',''),
+	(14,'deleted','');
+
 -- Add event_id column to thread_events
 ALTER TABLE `%TABLE_PREFIX%thread_event`
     ADD `event_id` int(11) unsigned AFTER `thread_id`;
