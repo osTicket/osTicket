f1f2f665434c08bf6ca05a85d5e883d825e43aee
Lucian Cristian <lucian.cristian@gmail.com>
2018-09-21 03:47:56 +0300

    [php7:warn] 7.2 : fix  count() warning in class.translation.php
    this is a try at annoyng: PHP Warning:  count(): Parameter must be an array or an object that implements Countable in .../upload/include/class.translation.php on line 674
    
    Signed-off-by: Lucian Cristian <lucian.cristian@gmail.com>

--- include/class.translation.php
+++ include/class.translation.php
@@ -672,7 +672,7 @@ class Translation extends gettext_reader implements Serializable {
         list($this->charset, $this->encode, $this->cache_translations)
             = unserialize($what);
         $this->short_circuit = ! $this->enable_cache
-            = 0 < count($this->cache_translations);
+            = 0 < is_array($this->cache_translations) && count($this->cache_translations);
     }
 }
 
