16e4472f1aae271c5e52e2ba04b8a9e4532dbc2c
Jared Hancock <gravydish@gmail.com>
2019-02-08 21:10:55 -0600

    queues: Fix compatibility issues with newer jQuery

--- include/staff/templates/queue-column.tmpl.php
+++ include/staff/templates/queue-column.tmpl.php
@@ -72,7 +72,8 @@ foreach (Internationalization::sortKeyedList($annotations) as $class=>$desc) {
       $(function() {
         var addAnnotation = function(type, desc, icon, pos) {
           var template = $('.annotation.template', '#annotations'),
-              clone = template.clone().show().removeClass('template').insertBefore(template),
+              clone = template.clone().removeClass('hidden')
+                  .removeClass('template').insertBefore(template),
               input = clone.find('[data-field=input]'),
               colid = clone.closest('.tab_content').data('colId'),
               column = clone.find('[data-field=column]'),
