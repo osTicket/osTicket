0da521ee3c93958e81f0c274476c744ce4ddbb71
JediKev <kevin@enhancesoft.com>
2019-04-17 16:05:53 -0500

    jquery: Update Again
    This updates jQuery to the latest stable release of v3.4.0.

--- include/client/header.inc.php
+++ include/client/header.inc.php
@@ -59,10 +59,14 @@ if ($lang) {
     <!-- Favicons -->
     <link rel="icon" type="image/png" href="<?php echo ROOT_PATH ?>images/oscar-favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="<?php echo ROOT_PATH ?>images/oscar-favicon-16x16.png" sizes="16x16" />
+<<<<<<< HEAD
     <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-ui-1.10.3.custom.min.js"></script>
 =======
     <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-3.3.1.min.js"></script>
+=======
+    <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-3.4.0.min.js"></script>
+>>>>>>> jquery: Update Again
     <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-ui-1.12.1.custom.min.js"></script>
 >>>>>>> jq: Update To jQuery-3.3.1
     <script src="<?php echo ROOT_PATH; ?>js/osticket.js"></script>
