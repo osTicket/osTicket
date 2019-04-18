33f586c8db00e406e928264d044c2946dc036763
aydreeihn <adriane@enhancesoft.com>
2019-04-18 13:43:26 -0500

    Bug fixes and enhancements for v1.11

--- scp/profile.php
+++ scp/profile.php
@@ -48,7 +48,8 @@ elseif($thisstaff->onVacation() && !$warn)
     $warn=sprintf(__("<b>Welcome back %s</b>! You are listed as 'on vacation' Please let your manager know that you are back."),$thisstaff->getFirstName());
 
 $inc='profile.inc.php';
-$nav->setTabActive('dashboard');
+if ($nav)
+    $nav->setTabActive('dashboard');
 $ost->addExtraHeader('<meta name="tip-namespace" content="dashboard.my_profile" />',
     "$('#content').data('tipNamespace', 'dashboard.my_profile');");
 require_once(STAFFINC_DIR.'header.inc.php');
