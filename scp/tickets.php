42b53c1e03bf05d000e1489603af0868124d3439
aydreeihn <adriane@enhancesoft.com>
2019-05-02 12:26:48 -0500

    Issue: Ticket View Permissions for Merged Tickets
    This commit changes merged Ticket permissions to the following:
    
    If the Agent does not have access to the Child ticket and the threads are merged, they should not be allowed to view the Parent. If the threads are not merged, the Agent should still be able to access the Parent Ticket.

--- scp/tickets.php
+++ scp/tickets.php
@@ -37,7 +37,7 @@ if($_REQUEST['id'] || $_REQUEST['number']) {
          $errors['err']=sprintf(__('%s: Unknown or invalid ID.'), __('ticket'));
     elseif($_REQUEST['number'] && !($ticket=Ticket::lookup(array('number' => $_REQUEST['number']))))
          $errors['err']=sprintf(__('%s: Unknown or invalid number.'), __('ticket'));
-    elseif(!$ticket->checkStaffPerm($thisstaff) || !$ticket->hasChildAccess()) {
+    elseif(!$ticket->checkStaffPerm($thisstaff) || (!$ticket->hasChildAccess() && $ticket->hasFlag(Ticket::FLAG_SHOW_CHILDREN))) {
         $errors['err']=__('Access denied. Contact admin if you believe this is in error');
         $ticket=null; //Clear ticket obj.
     }
