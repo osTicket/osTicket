1c6d64dbf43c99131fb4d3b587991c03ac61ab76
Peter Rotich <peter@enhancesoft.com>
2019-07-15 20:47:15 +0000

    DatetimeField: Use in-line time picker
    - Use datetimepicker (jquery-ui-timepicker-addon) for Datetime Field with time
    selection
    - Makes sure the front-end returns ISO formatted date to standardize how dates
    are handled
    - Fix javascript date format translation from php formats

--- scp/js/scp.js
+++ scp/js/scp.js
@@ -553,10 +553,7 @@ var fixupDatePickers = function() {
         var $e = $(e),
             d = $e.datepicker('getDate');
         if (!d || $e.data('fixed')) return;
-        var day = ('0'+d.getDate()).substr(-2),
-            month = ('0'+(d.getMonth()+1)).substr(-2),
-            year = d.getFullYear();
-        $e.val(year+'-'+month+'-'+day);
+        $e.val(d.toISOString());
         $e.data('fixed', true);
         $e.on('change', function() { $(this).data('fixed', false); });
     });
@@ -611,7 +608,6 @@ $.translate_format = function(str) {
         'yyyy': '`',
         'yyy':  '`',
         'yy':   'y',
-        'y':    'yy',
         '`':    'yy'
     };
     // Change PHP formats to datepicker ones
