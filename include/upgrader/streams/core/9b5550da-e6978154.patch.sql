94e770acb6172c6761a5463c87ff53d246d2509d
JediKev <kevin@enhancesoft.com>
2019-09-04 11:27:27 -0500

    issue: Form Field Flags
    This addresses an issue where any Agent can change the Type and Variable of
    some of the core fields. In most cases, changing the Type and Variable of
    core fields will cause issues later on down the line. With this in mind, we
    need to set the flags to where Agents cannot change the Type and Variable of
    specific core fields to avoid any issues. This updates the installer to
    reflect the additional flags and updates the Upgrader to add these flags to
    existing fields.

--- include/upgrader/streams/core/9b5550da-e6978154.patch.sql
+++ include/upgrader/streams/core/9b5550da-e6978154.patch.sql
@@ -49,6 +49,43 @@ JOIN `tmp_table` A2 ON A2.thread_id = A1.thread_id
 SET A2.thread_id = 0
 WHERE A1.event_id = 14;
 
+-- Organization / Name
+UPDATE `%TABLE_PREFIX%_form_field` A1
+JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'O')
+SET A1.`flags` = A1.`flags` | 16, A1.`type` = 'text'
+WHERE A1.`name` = 'name';
+
+-- Contact Information / Name, Email
+UPDATE `%TABLE_PREFIX%_form_field` A1
+JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'U')
+SET A1.`flags` = A1.`flags` | 16
+WHERE (A1.`name` = 'name' OR A1.`name` = 'email')
+AND A1.`type` = 'text';
+
+-- Ticket Details / Issue Summary
+UPDATE `%TABLE_PREFIX%_form_field` A1
+JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'T')
+SET A1.`flags` = A1.`flags` | 16
+WHERE A1.`name` = 'subject' AND A1.`type` = 'text';
+
+-- Task Details / Title
+UPDATE `%TABLE_PREFIX%_form_field` A1
+JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'A')
+SET A1.`flags` = A1.`flags` | 16
+WHERE A1.`name` = 'title' AND A1.`type` = 'text';
+
+-- Company Information / Name
+UPDATE `%TABLE_PREFIX%_form_field` A1
+JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'C')
+SET A1.`flags` = A1.`flags` | 16
+WHERE A1.`name` = 'name' AND A1.`type` = 'text';
+
+-- Company Information / Website, Phone Number
+UPDATE `%TABLE_PREFIX%_form_field` A1
+JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'C')
+SET A1.`flags` = A1.`flags` | 262160
+WHERE (A1.`name` = 'website' AND A1.`type` = 'text') OR (A1.`name` = 'phone' AND A1.`type` = 'phone');
+
 -- Finished with patch
 UPDATE `%TABLE_PREFIX%config`
    SET `value` = 'e69781546e08be96d787199a911d0ffe', `updated` = NOW()
