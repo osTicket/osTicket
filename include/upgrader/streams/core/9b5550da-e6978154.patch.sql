00d30c6811767b0410414684f8463fb717be549d
JediKev <kevin@enhancesoft.com>
2019-09-06 13:00:25 -0500

    issue: Fix Patch Issue
    This addresses an issue where resolving conflicts on Git instead of rebasing
    caused a file to appear that wasn’t supposed to. This also addresses a typo
    where there were extra underscores in the table names.

--- include/upgrader/streams/core/9b5550da-e6978154.patch.sql
+++ include/upgrader/streams/core/9b5550da-e6978154.patch.sql
@@ -82,44 +82,45 @@ SET A2.thread_id = 0
 WHERE A1.event_id = 14;
 
 -- Organization / Name
-UPDATE `%TABLE_PREFIX%_form_field` A1
-JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'O')
+UPDATE `%TABLE_PREFIX%form_field` A1
+JOIN `%TABLE_PREFIX%form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'O')
 SET A1.`flags` = A1.`flags` | 16, A1.`type` = 'text'
 WHERE A1.`name` = 'name';
 
 -- Contact Information / Name, Email
-UPDATE `%TABLE_PREFIX%_form_field` A1
-JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'U')
+UPDATE `%TABLE_PREFIX%form_field` A1
+JOIN `%TABLE_PREFIX%form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'U')
 SET A1.`flags` = A1.`flags` | 16
 WHERE (A1.`name` = 'name' OR A1.`name` = 'email')
 AND A1.`type` = 'text';
 
 -- Ticket Details / Issue Summary
-UPDATE `%TABLE_PREFIX%_form_field` A1
-JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'T')
+UPDATE `%TABLE_PREFIX%form_field` A1
+JOIN `%TABLE_PREFIX%form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'T')
 SET A1.`flags` = A1.`flags` | 16
 WHERE A1.`name` = 'subject' AND A1.`type` = 'text';
 
 -- Task Details / Title
-UPDATE `%TABLE_PREFIX%_form_field` A1
-JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'A')
+UPDATE `%TABLE_PREFIX%form_field` A1
+JOIN `%TABLE_PREFIX%form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'A')
 SET A1.`flags` = A1.`flags` | 16
 WHERE A1.`name` = 'title' AND A1.`type` = 'text';
 
 -- Company Information / Name
-UPDATE `%TABLE_PREFIX%_form_field` A1
-JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'C')
+UPDATE `%TABLE_PREFIX%form_field` A1
+JOIN `%TABLE_PREFIX%form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'C')
 SET A1.`flags` = A1.`flags` | 16
 WHERE A1.`name` = 'name' AND A1.`type` = 'text';
 
 -- Company Information / Website, Phone Number
-UPDATE `%TABLE_PREFIX%_form_field` A1
-JOIN `%TABLE_PREFIX%_form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'C')
+UPDATE `%TABLE_PREFIX%form_field` A1
+JOIN `%TABLE_PREFIX%form` A2 ON (A1.`form_id` = A2.`id` AND A2.`type` = 'C')
 SET A1.`flags` = A1.`flags` | 262160
 WHERE (A1.`name` = 'website' AND A1.`type` = 'text') OR (A1.`name` = 'phone' AND A1.`type` = 'phone');
 
 -- Finished with patch
 UPDATE `%TABLE_PREFIX%config`
+<<<<<<< HEAD
    SET `value` = 'e69781546e08be96d787199a911d0ffe', `updated` = NOW()
    WHERE `key` = 'schema_signature' AND `namespace` = 'core';
 =======
@@ -143,3 +144,7 @@ UPDATE `%TABLE_PREFIX%config`
 SET `value` = 'e69781546e08be96d787199a911d0ffe', `updated` = NOW()
 WHERE `key` = 'schema_signature' AND `namespace` = 'core';
 >>>>>>> Add thread_type Patch
+=======
+  SET `value` = 'e69781546e08be96d787199a911d0ffe', `updated` = NOW()
+  WHERE `key` = 'schema_signature' AND `namespace` = 'core';
+>>>>>>> issue: Fix Patch Issue
