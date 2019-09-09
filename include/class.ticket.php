24b220de066f8781fe321fa2b9fd442483191152
aydreeihn <adriane@enhancesoft.com>
2019-09-09 12:14:20 -0500

    Don't Delete Child Threads
    If we choose to delete a child ticket during merging, we should still keep the child thread so that we have the merge indicator on the parent ticket to show us which threads belonged to the deleted child and which belong to the parent ticket.

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -3462,8 +3462,8 @@ implements RestrictedAccess, Threadable, Searchable {
                 $parent->setMergeType(3);
                 $parent->save();
             }
-        }
-
+        } else
+            $t->delete();
 
         $this->logEvent('deleted');
 
