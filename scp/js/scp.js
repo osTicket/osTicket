66df29a8a061eb7c7a5f67d5199327f94d645483
aydreeihn <adriane@enhancesoft.com>
2019-09-13 09:40:54 -0500

    European Date Format Issue
    This commit fixes an issue where if your date format is dd/MM/y h:mm a, the date picker will show the wrong year because the dates were not translating correctly between PHP and JS.

--- scp/js/scp.js
+++ scp/js/scp.js
@@ -609,6 +609,7 @@ $.translate_format = function(str) {
         'yyyy': '`',
         'yyy':  '`',
         'yy':   'y',
+        'y':    'yy',
         '`':    'yy'
     };
     // Change PHP formats to datepicker ones
