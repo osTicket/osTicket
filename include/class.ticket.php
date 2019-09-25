8c087f4b6d886cb82d6cf8ac092c695aabaa9783
aydreeihn <adriane@enhancesoft.com>
2019-09-25 09:33:48 -0500

    Issue: Unlinking From Child Ticket
    Make sure we account for unlinking within a ticket using an AJAX call.

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -2432,7 +2432,7 @@ implements RestrictedAccess, Threadable, Searchable {
                 $child = Ticket::lookup($child[0]);
                 $child->unlinkChild($parent);
             }
-        } else
+        } elseif ($child)
             $child->unlinkChild($parent);
 
         if (count(Ticket::getChildTickets($pid)) == 0) {
