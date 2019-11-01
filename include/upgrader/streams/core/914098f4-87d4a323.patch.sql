c6adff90787b263e27e45abdeec815ce6db179dd
JediKev <kevin@enhancesoft.com>
2019-11-01 09:17:16 -0500

    issue: Add Default Embed Whitelist Domains
    This adds the default Domains for the Embed Whitelist feature.

--- include/upgrader/streams/core/914098f4-87d4a323.patch.sql
+++ include/upgrader/streams/core/914098f4-87d4a323.patch.sql
@@ -14,6 +14,12 @@ UPDATE `%TABLE_PREFIX%role` A1
     SET A1.`permissions` = REPLACE(A1.`permissions`, '"ticket.reply":1,', '"ticket.reply":1,"ticket.markanswered":1,')
     WHERE A1.`permissions` LIKE '%"ticket.reply":1%';
 
+-- Add Default Domains For Embed Whitelist
+INSERT INTO `%TABLE_PREFIX%config` (`id`, `namespace`, `key`, `value`, `updated`) VALUES
+    ('', 'core', 'embedded_domain_whitelist', 'youtube.com, dailymotion.com, vimeo.com, player.vimeo.com, web.microsoftstream.com', NOW())
+ON DUPLICATE KEY UPDATE
+    `value` = 'youtube.com, dailymotion.com, vimeo.com, player.vimeo.com, web.microsoftstream.com', `updated` = NOW();
+
 -- Finished with patch
 UPDATE `%TABLE_PREFIX%config`
     SET `value` = '87d4a3233469728d83b86d3ee2f066e1'
