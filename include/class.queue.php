1a32e2a9abd0fe6fdce36182651077e8e9add05b
JediKev <kevin@enhancesoft.com>
2019-12-30 09:41:36 -0600

    queue: Parent/Child "Inherit Columns" Option
    This addresses an issue where saving a Parent Queue with "Use standard
    columns" Enabled will show the option Enabled but still show the
    customizable Columns instead of the Standard Columns. This addresses an
    issue where saving a Child Queue with "Inherit columns from the parent
    queue" Enabled shows the Standard Columns but does not show the option
    Enabled. This adds a check in `inheritColumns()` for Staff Configurations
    for the queue and if exists we use the Staff preferences, else we default to
    the Queue Flag check. This adds a new method called `staffSettings()` that
    returns staff settings for a queue. It takes an optional argument (a
    setting's name) that can return a specific setting's value.

--- include/class.queue.php
+++ include/class.queue.php
@@ -1015,6 +1015,12 @@ class CustomQueue extends VerySimpleModel {
     }
 
     function inheritColumns() {
+        global $thisstaff;
+
+        if ($thisstaff && ($inherit = self::staffSettings('inherit-columns'))
+                && !is_null($inherit))
+            return $inherit == 'true';
+
         return $this->hasFlag(self::FLAG_INHERIT_COLUMNS);
     }
 
@@ -1033,6 +1039,22 @@ class CustomQueue extends VerySimpleModel {
 
     function isDefaultSortInherited() {
         return $this->hasFlag(self::FLAG_INHERIT_DEF_SORT);
+    }
+
+    function staffSettings($key=null) {
+        global $thisstaff;
+
+        if (!$config = QueueConfig::objects()->filter(array(
+                'staff_id' => $thisstaff->getId(),
+                'queue_id' => $this->getId()))->first())
+            return null;
+
+        $settings = JsonDataParser::decode($config->setting) ?: null;
+
+        if ($key && $settings)
+            return $settings[$key];
+
+        return $settings;
     }
 
     function buildPath() {
