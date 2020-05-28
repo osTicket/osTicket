ea0937384dc5033b3266d85c83ac9d1b2fe30cfa
JediKev <kevin@enhancesoft.com>
2020-05-28 11:57:11 -0500

    upgrade: Add Version Column To Plugin Table
    This addresses an issue where we've never added the `version` column to any
    of the upgrade patches so if someone were to upgrade from pre-1.8, they will
    not have the `version` column and they will not be able to install plugins.
    This adds a prepared statement to check if the column already exists, if it
    does not exists we will add the column. We have to use a prepared statement
    as unfortunately MySQL doesn’t have the concept of `ADD COLUMN IF NOT
    EXISTS` like MariaDB does.

--- include/upgrader/streams/core/e7038ce9-8cd26958.patch.sql
+++ include/upgrader/streams/core/e7038ce9-8cd26958.patch.sql
@@ -3,7 +3,8 @@
  * @version v1.15
  * @title Add indexes accumulated over time
  *
- * This patch adds all indexes we've implemented over time
+ * This patch adds all indexes we've implemented over time and adds the
+ * `version` column to the plugin table (if not exists)
  */
 
 -- Add FLAG_AGENT_REQUIRED For Task Title
@@ -25,6 +26,20 @@ WHERE `form_id` = (SELECT `id` FROM `%TABLE_PREFIX%form` WHERE `type` = 'A') AND
 UPDATE `%TABLE_PREFIX%form_field`
 SET `flags` = IF((`flags` & 131072) != 131072, `flags` | 131072, `flags`)
 WHERE `form_id` = (SELECT `id` FROM `%TABLE_PREFIX%form` WHERE `type` = 'A') AND `name` IN ('title', 'description');
+
+-- Add `version` column to the plugin table (if not exists)
+SET @s = (SELECT IF(
+    (SELECT COUNT(*)
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE table_name = '%TABLE_PREFIX%plugin'
+        AND table_schema = DATABASE()
+        AND column_name = 'version'
+    ) > 0,
+    "SELECT 1",
+    "ALTER TABLE `%TABLE_PREFIX%plugin` ADD `version` VARCHAR(64) DEFAULT NULL AFTER `isactive`"
+));
+PREPARE stmt FROM @s;
+EXECUTE stmt;
 
 -- Finished with patch
 UPDATE `%TABLE_PREFIX%config`
