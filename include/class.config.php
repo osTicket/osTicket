4ef752c68e1cf6b5839c50d9ce56215a987112ed
Peter Rotich <peter@osticket.com>
2020-07-03 07:48:53 +0000

    Add setting to require 2FA
    This commit adds setting to Admin Panel > Settings > Agents to force
    agents to setup 2FA.

--- include/class.config.php
+++ include/class.config.php
@@ -465,6 +465,10 @@ class OsticketConfig extends Config {
         return $this->get('client_passwd_policy');
     }
 
+    function require2FAForAgents() {
+         return $this->get('require_agent_2fa');
+    }
+
     function isRichTextEnabled() {
         return $this->get('enable_richtext');
     }
@@ -1334,6 +1338,7 @@ class OsticketConfig extends Config {
             'staff_ip_binding'=>isset($vars['staff_ip_binding'])?1:0,
             'allow_pw_reset'=>isset($vars['allow_pw_reset'])?1:0,
             'pw_reset_window'=>$vars['pw_reset_window'],
+            'require_agent_2fa'=>$vars['require_agent_2fa'],
             'agent_name_format'=>$vars['agent_name_format'],
             'hide_staff_name'=>isset($vars['hide_staff_name']) ? 1 : 0,
             'agent_avatar'=>$vars['agent_avatar'],
