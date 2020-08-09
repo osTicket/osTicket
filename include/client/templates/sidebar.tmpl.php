1cea8ddfaa3485ab5d5fe599f7d0f9be12e08db2
msolis <51387932+solisman@users.noreply.github.com>
2020-08-08 22:18:56 -0500

    update tables to bootstrap grid

--- include/client/templates/sidebar.tmpl.php
+++ include/client/templates/sidebar.tmpl.php
@@ -1,46 +1,55 @@
 <?php
 $BUTTONS = isset($BUTTONS) ? $BUTTONS : true;
 ?>
-    <div class="sidebar pull-right">
-<?php if ($BUTTONS) { ?>
-        <div class="front-page-button flush-right">
-<p>
-<?php
-    if ($cfg->getClientRegistrationMode() != 'disabled'
-        || !$cfg->isClientLoginRequired()) { ?>
-            <a href="open.php" style="display:block" class="blue button"><?php
+    <div class="container">
+		<?php if ($BUTTONS) { ?>
+      <div class="row align-items-start">
+      <!-- "front-page-button flush-right" -->
+			
+			<?php
+    		if ($cfg->getClientRegistrationMode() != 'disabled'
+      	  || !$cfg->isClientLoginRequired()) { ?>
+      	<div class="col-md-6" style="padding-top: 1rem; padding-bottom: 1rem;">
+            <a href="open.php" style="display:block" class="btn btn-md btn-outline-info">
+				<i class="fa fa-plus " style=" font-size: 1rem;"></i>            
+            <?php
                 echo __('Open a New Ticket');?></a>
-</p>
-<?php } ?>
-<p>
-            <a href="view.php" style="display:block" class="green button"><?php
-                echo __('Check Ticket Status');?></a>
-</p>
-        </div>
-<?php } ?>
-        <div class="content"><?php
-    if ($cfg->isKnowledgebaseEnabled()
-        && ($faqs = FAQ::getFeatured()->select_related('category')->limit(5))
-        && $faqs->all()) { ?>
-            <section><div class="header"><?php echo __('Featured Questions'); ?></div>
-<?php   foreach ($faqs as $F) { ?>
-            <div><a href="<?php echo ROOT_PATH; ?>kb/faq.php?id=<?php
-                echo urlencode($F->getId());
-                ?>"><?php echo $F->getLocalQuestion(); ?></a></div>
-<?php   } ?>
+			</div>
+			<?php } ?>
+			<div class="col-md-6" style="padding-top: 1rem; padding-bottom: 1rem;">
+         	<a href="view.php" style="display:block" class="btn btn-md btn-outline-success">
+         	<i class="fa fa-search " style=" font-size: 1rem;"></i>  
+         	<?php
+            	echo __('Check Ticket Status');?></a>
+			</div>
+		</div>
+		<?php } ?>
+      <div class="content"><?php
+    		if ($cfg->isKnowledgebaseEnabled()
+        	&& ($faqs = FAQ::getFeatured()->select_related('category')->limit(5))
+        	&& $faqs->all()) { ?>
+         	<section><div class="header"><?php echo __('Featured Questions'); ?></div>
+					<?php   foreach ($faqs as $F) { ?>
+            		<div><a href="<?php echo ROOT_PATH; ?>kb/faq.php?id=<?php
+               	 	echo urlencode($F->getId());
+               	 	?>"><?php echo $F->getLocalQuestion(); ?></a></div>
+					<?php   } ?>
             </section>
-<?php
-    }
-    $resources = Page::getActivePages()->filter(array('type'=>'other'));
-    if ($resources->all()) { ?>
-            <section><div class="header"><?php echo __('Other Resources'); ?></div>
-<?php   foreach ($resources as $page) { ?>
-            <div><a href="<?php echo ROOT_PATH; ?>pages/<?php echo $page->getNameAsSlug();
-            ?>"><?php echo $page->getLocalName(); ?></a></div>
-<?php   } ?>
+				<?php
+    		}
+    		$resources = Page::getActivePages()->filter(array('type'=>'other'));
+    		if ($resources->all()) { ?>
+         	<section><div class="header"><?php echo __('Other Resources'); ?></div>
+					<?php   foreach ($resources as $page) { ?>
+            		<div>
+            			<a href="<?php echo ROOT_PATH; ?>pages/<?php echo $page->getNameAsSlug();
+            			?>"><?php echo $page->getLocalName(); ?></a>
+            		</div>
+					<?php   } ?>
             </section>
-<?php
-    }
-        ?></div>
+			<?php
+    		}
+        ?>
+		</div>
     </div>
 
