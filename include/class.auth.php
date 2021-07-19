osTicket/osTicket

commit b755b990eca64f2d720b8f926c2078a62bc7db23

Auth: Service Name on Client Portal
Use configurable service name when supported by the auth backend.

include/class.auth.php
@@ -847,19 +847,24 @@ abstract class ExternalUserAuthenticationBackend
     static $sign_in_image_url = false;
     static $service_name = "External";
 
-    function renderExternalLink() { ?>
-        <a class="external-sign-in" title="<?php echo sprintf(__('Sign in with %s'), __(static::$service_name)); ?>"
+    function renderExternalLink() {
+        $service = sprintf('%s %s',
+                __('Sign in with'),
+                $this->getServiceName());
+
+        ?>
+        <a class="external-sign-in" title="<?php echo $service; ?>"
                 href="login.php?do=ext&amp;bk=<?php echo urlencode(static::$id); ?>">
 <?php if (static::$sign_in_image_url) { ?>
         <img class="sign-in-image" src="<?php echo static::$sign_in_image_url;
-            ?>" alt="<?php echo sprintf(__('Sign in with %s'), __(static::$service_name)); ?>"/>
+            ?>" alt="<?php $service; ?>"/>
 <?php } else { ?>
             <div class="external-auth-box">
             <span class="external-auth-icon">
                 <i class="icon-<?php echo static::$fa_icon; ?> icon-large icon-fixed-with"></i>
             </span>
             <span class="external-auth-name">
-                <?php echo sprintf(__('Sign in with %s'), __(static::$service_name)); ?>
+                <?php echo $service; ?>
             </span>
             </div>
 <?php } ?>

commit da05573f8c31b5e579518814234f3506b335c706

Auth: Service Name
This commit addresses an issue introduced by commit b755b990.

include/class.auth.php
@@ -461,6 +461,13 @@ interface ExternalAuthentication {
     function renderExternalLink();
 
     /**
+     * Function: getServiceName
+     *
+     * Called to get the service name displayed on login page.
+     */
+     function getServiceName();
+
+    /**
      * Function: triggerAuth
      *
      * Called when a user clicks the button rendered in the
@@ -643,7 +650,7 @@ abstract class ExternalStaffAuthenticationBackend
     static $service_name = "External";
 
     function getServiceName() {
-        return static::$service_name;
+        return __(static::$service_name);
     }
 
     function renderExternalLink() {
@@ -846,6 +853,10 @@ abstract class ExternalUserAuthenticationBackend
     static $fa_icon = "signin";
     static $sign_in_image_url = false;
     static $service_name = "External";
+
+    function getServiceName() {
+        return __(static::$service_name);
+    }
 
     function renderExternalLink() {
         $service = sprintf('%s %s',