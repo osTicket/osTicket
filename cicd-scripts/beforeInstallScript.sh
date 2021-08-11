ed0656a38a3f7eb1c6065e84e407132dd82669ef
naveen-stackpro <88052102+naveen-stackpro@users.noreply.github.com>
2021-08-11 21:11:17 +0530

    Added CICD scripts for cleanup

---
+++ cicd-scripts/beforeInstallScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ -d /var/www/html/osTicket ]; then
+    sudo rm -rf /var/www/html/osTicket
+fi
+sudo mkdir -vp /var/www/html/osTicket
---
+++ cicd-scripts/cleanSetupScript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ -d /var/www/html/osTicket/setup ]; then
+    sudo rm -rf /var/www/html/osTicket/setup
+fi
