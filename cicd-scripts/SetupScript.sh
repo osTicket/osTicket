f03b00cf5d38d1e7e02d509cb792f6476cbcefaa
naveen-stackpro <88052102+naveen-stackpro@users.noreply.github.com>
2021-08-13 18:20:38 +0530

    Removed setup from osticket

--- cicd-scripts/SetupScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -d /var/www/html/osTicket/setup ]; then
-    sudo rm -rf /var/www/html/osTicket/setup
-fi
-
-aws s3 cp s3://codepipeline-ap-south-1-821620239570/config-files/osTicket-stage/ost-config.php /var/www/html/osTicket/include/
-
