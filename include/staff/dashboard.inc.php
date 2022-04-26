1ebafd350008712d1c8b9ca29bd34ded3d1ffa42
sairentov <43753748+sairentov@users.noreply.github.com>
2022-04-26 05:08:17 +0200

    Remove version from file links ("?29ab0c5")

--- include/staff/dashboard.inc.php
+++ include/staff/dashboard.inc.php
@@ -3,13 +3,13 @@ $report = new OverviewReport($_POST['start'], $_POST['period']);
 $plots = $report->getPlotData();
 
 ?>
-<script type="text/javascript" src="js/raphael-min.js?39ab0c5"></script>
-<script type="text/javascript" src="js/g.raphael.js?39ab0c5"></script>
-<script type="text/javascript" src="js/g.line-min.js?39ab0c5"></script>
-<script type="text/javascript" src="js/g.dot-min.js?39ab0c5"></script>
-<script type="text/javascript" src="js/dashboard.inc.js?39ab0c5"></script>
+<script type="text/javascript" src="js/raphael-min.js"></script>
+<script type="text/javascript" src="js/g.raphael.js"></script>
+<script type="text/javascript" src="js/g.line-min.js"></script>
+<script type="text/javascript" src="js/g.dot-min.js"></script>
+<script type="text/javascript" src="js/dashboard.inc.js"></script>
 
-<link rel="stylesheet" type="text/css" href="css/dashboard.css?39ab0c5"/>
+<link rel="stylesheet" type="text/css" href="css/dashboard.css"/>
 
 <form method="post" action="dashboard.php">
 <div id="basic_search">
--- include/staff/header.inc.php
+++ include/staff/header.inc.php
@@ -32,25 +32,25 @@ if (osTicket::is_ie())
         .tip_shadow { display:block !important; }
     </style>
     <![endif]-->
-    <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-3.5.1.min.js?39ab0c5"></script>
+    <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/colorthemes/default-material-blue-amber.css" media="all"/>
-	<link rel="stylesheet" href="<?php echo ROOT_PATH ?>css/thread.css?39ab0c5" media="all"/>
-	<link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/scp.css?39ab0c5" media="all"/>
-    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/redactor.css?39ab0c5" media="screen"/>
-    <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/typeahead.css?39ab0c5" media="screen"/>
-    <link type="text/css" href="<?php echo ROOT_PATH; ?>css/ui-lightness/jquery-ui-1.10.3.custom.min.css?39ab0c5"
+	<link rel="stylesheet" href="<?php echo ROOT_PATH ?>css/thread.css" media="all"/>
+	<link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/scp.css" media="all"/>
+    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/redactor.css" media="screen"/>
+    <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/typeahead.css" media="screen"/>
+    <link type="text/css" href="<?php echo ROOT_PATH; ?>css/ui-lightness/jquery-ui-1.10.3.custom.min.css"
          rel="stylesheet" media="screen" />
-    <link rel="stylesheet" href="<?php echo ROOT_PATH ?>css/jquery-ui-timepicker-addon.css?39ab0c5" media="all"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome.min.css?39ab0c5"/>
+    <link rel="stylesheet" href="<?php echo ROOT_PATH ?>css/jquery-ui-timepicker-addon.css" media="all"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome.min.css"/>
     <!--[if IE 7]>
-    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome-ie7.min.css?39ab0c5"/>
+    <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome-ie7.min.css"/>
     <![endif]-->
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/dropdown.css?39ab0c5"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/loadingbar.css?39ab0c5"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/flags.css?39ab0c5"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/select2.min.css?39ab0c5"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/rtl.css?39ab0c5"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/translatable.css?39ab0c5"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/dropdown.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/loadingbar.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/flags.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/select2.min.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/rtl.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/translatable.css"/>
     <!-- Favicons -->
     <link rel="icon" type="image/png" href="<?php echo ROOT_PATH ?>images/oscar-favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="<?php echo ROOT_PATH ?>images/oscar-favicon-16x16.png" sizes="16x16" />
