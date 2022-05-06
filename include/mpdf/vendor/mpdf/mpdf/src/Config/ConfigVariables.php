38c0979e216fd159daf63eb3b0e305d61485976e
JediKev <kevin@enhancesoft.com>
2022-05-06 15:46:00 +0000

    issue: mPDF Table Print
    This addresses issue 6159 where tables are not appearing correctly in PDFs.
    This is due to mPDF config revert when upgrading mPDF. This updates the
    config `ignore_table_widths` to `true` so tables appear correctly.

--- include/mpdf/vendor/mpdf/mpdf/src/Config/ConfigVariables.php
+++ include/mpdf/vendor/mpdf/mpdf/src/Config/ConfigVariables.php
@@ -261,7 +261,7 @@ class ConfigVariables
 			'packTableData' => false,
 
 			'ignore_table_percents' => false,
-			'ignore_table_widths' => false,
+			'ignore_table_widths' => true,
 			// If table width set > page width, force resizing but keep relative sizes
 			// Also forces respect of cell widths set by %
 			'keep_table_proportions' => true,
