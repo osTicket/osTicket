50bc1a213334394370967a655e4424fa6c1b066d
JediKev <kevin@enhancesoft.com>
2022-11-02 20:23:02 +0000

    issue: exif_imagetype()
    This addresses an issue where if someone doesn't install the PHP `exif`
    extension, subsequent calls to `exif_imagetype()` returns `undefined`
    errors. This checks to see if the method is defined and if not defines it.

--- bootstrap.php
+++ bootstrap.php
@@ -45,6 +45,15 @@ class Bootstrap {
         }
         date_default_timezone_set('UTC');
 
+        if (!function_exists('exif_imagetype')) {
+            function exif_imagetype ($filename) {
+                if ((list($width,$height,$type,) = getimagesize($filename)) !== false)
+                    return $type;
+
+                return false;
+            }
+        }
+
         if (!isset($_SERVER['REMOTE_ADDR']))
             $_SERVER['REMOTE_ADDR'] = '';
     }
