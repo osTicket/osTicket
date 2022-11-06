osTicket/osTicket

commit f1639d4123ed31c87f8dc906697210664821862a

fetcher: Mail Fetch Order
osTicket currently fetches messages in LIFO order meaning the most recent messages get
fetched first. This was intentional to avoid possible stalemate when fetched
messages are left in the inbox (Do Nothing Option). However, this can creates
out of order threading issues since subsequest replies can be fetched first.

This commits addresses the issue by changing the logic so we can fetch older
messages first (FIFO order) as long as the numbers of the emails in the folder
being fetched are less than maximum messages to process at a time.

include/class.mailfetch.php
@@ -93,21 +93,36 @@ class Fetcher {
 
         // Get basic fetch settings
         $archiveFolder = $this->getArchiveFolder();
-        $delete = $this->canDeleteEmails();
-        $max = $this->getMaxFetch();
-        // Get full message count
-        $messageCount = $this->mbox->countMessages();
+        $deleteFetched =  $this->canDeleteEmails();
+        $max = $this->getMaxFetch() ?: 30; // default to 30 if not set
+
+        // Get message count in the Fetch Folder
+        if (!($messageCount = $this->mbox->countMessages()))
+            return 0;
+
+        // Create a range of message sequence numbers (msgno) to fetch
+        // taking max fetch into account
+        // TODO: Use message UIDs instead of numbers
+        $messages = range(1, min($max, $messageCount));
+        // If the number of emails in the folder are more than Max Fetch
+        // then fetch in Reverse Order - this is necessary when fetched
+        // emails are not getting archived or deleted, which might lead to
+        // fetcher being stuck 4ever on processing old emails already
+        // fetched
+        if ($messageCount > $max)
+            rsort($messages, SORT_NUMERIC);
+
         $msgs = $errors = 0;
-        for ($i = $messageCount; $i > 0; $i--) { // Process messages in reverse.
+        foreach ($messages as $i) {
             try {
                 // Okay, let's try to create a ticket
-                if ($this->createTicket($i)) {
+                if (($result=$this->createTicket($i))) {
                     // Mark the message as "Seen" (IMAP only)
                     $this->mbox->markAsSeen($i);
                     // Attempt to move the message if archive folder is set or
                     if ($archiveFolder)
                         $this->mbox->moveMessage($i, $archiveFolder);
-                    elseif ($delete)  // else delete if deletion is desired
+                    elseif ($deleteFetched)  // else delete if deletion is desired
                         $this->mbox->removeMessage($i);
                     $msgs++;
                     $errors = 0; // We are only interested in consecutive errors.
@@ -115,16 +130,17 @@ class Fetcher {
                     $errors++;
                 }
             } catch (\Throwable $t) {
-                // Generic Exceptions - log the message
-                $errors++;
+                // If we have result then exception happened after email
+                // processing and shouldn't count as an error
+                if (!$result)
+                    $errors++;
+                // log the exception as a debug message
                 $this->logDebug($t->getMessage());
             }
-
-            if ($max && ($msgs >= $max || $errors > ($max*0.8)))
-                break;
         }
+        // Expunge the mailbox
 		$this->mbox->expunge();
-        // Warn on excessive errors
+        // Warn on excessive errors (errors more than tickets created)
         if ($errors > $msgs) {
             $warn = sprintf(_S('Excessive errors processing emails for %1$s (%2$s). Please manually check the inbox.'),
                     $this->mbox->getHostInfo(), $this->getEmail());

commit 24338c2b5daa6f1d4a8c5ddf77924d8e921fefb7

fetcher: Max Fetch vs. Mail Box Count
This commit addresses a logic bug introduced in commit f1639d4123

include/class.mailfetch.php
@@ -100,19 +100,22 @@ class Fetcher {
         if (!($messageCount = $this->mbox->countMessages()))
             return 0;
 
-        // Create a range of message sequence numbers (msgno) to fetch
-        // taking max fetch into account
-        // TODO: Use message UIDs instead of numbers
-        $messages = range(1, min($max, $messageCount));
         // If the number of emails in the folder are more than Max Fetch
-        // then fetch in Reverse Order - this is necessary when fetched
-        // emails are not getting archived or deleted, which might lead to
-        // fetcher being stuck 4ever on processing old emails already
+        // then process the latest $max emails - this is necessary when
+        // fetched emails are not getting archived or deleted, which might
+        // lead to fetcher being stuck 4ever processing old emails already
         // fetched
-        if ($messageCount > $max)
-            rsort($messages, SORT_NUMERIC);
+        if ($messageCount > $max) {
+            // Latest $max messages
+            $messages = range($messageCount-$max, $messageCount);
+        } else {
+            // Create a range of message sequence numbers (msgno) to fetch
+            // starting from the oldest taking max fetch into account
+            $messages = range(1, min($max, $messageCount));
+        }
 
         $msgs = $errors = 0;
+        // TODO: Use message UIDs instead of ids
         foreach ($messages as $i) {
             try {
                 // Okay, let's try to create a ticket