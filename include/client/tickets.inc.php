50dfe93133954cf56ee08469aa97de2c93c7b51e
JediKev <kevin@enhancesoft.com>
2022-11-10 09:23:05 -0600

    issue: Html Typo
    This addresses a small typo where the class `Html` was used instead of
    `Http`.

--- include/client/tickets.inc.php
+++ include/client/tickets.inc.php
@@ -171,7 +171,7 @@ foreach (Topic::getHelpTopics(true) as $id=>$name) {
 
 
 <h1 style="margin:10px 0">
-    <a href="<?php echo Html::refresh_url(); ?>"
+    <a href="<?php echo Http::refresh_url(); ?>"
         ><i class="refresh icon-refresh"></i>
     <?php echo __('Tickets'); ?>
     </a>
