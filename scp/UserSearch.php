50aa37b19697df1cd1b13d8726d6445cde74729f
Antt1995 <antt1995@antts.uk>
2024-11-06 10:46:23 +0000

    🔧 Fix: Update phone links for better accessibility
    Updated phone links to use 'tel' instead of 'callto' for improved usability and accessibility.

--- scp/UserSearch.php
+++ scp/UserSearch.php
@@ -709,7 +709,7 @@ body {
 												<i class="bi bi-pencil-square pe-none"></i>
 												</div>
 												&nbsp&nbsp
-												<a href="callto:'.$row["UserPhone"].'">
+												<a href="tel:'.$row["UserPhone"].'">
 													<i class="bi bi-telephone-outbound-fill"></i>
 												</a>
 											</div>';
@@ -744,7 +744,7 @@ body {
 													<i class="bi bi-pencil-square"></i>
 												</div>
 												&nbsp&nbsp
-												<a href="callto:'.$row["OrgPhone"].'">
+												<a href="tel:'.$row["OrgPhone"].'">
 													<i class="bi bi-telephone-outbound-fill"></i>
 												</a>
 											</div>
