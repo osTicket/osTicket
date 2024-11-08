a6ea2eaca02a163ef556e7d9db0f6ef3ee37512d
Antt1995 <antt1995@antts.uk>
2024-11-08 12:16:19 +0000

    🔧 Adjusted icon color in auth_img function
    Changed icon color to secondary for better visibility.

--- scp/includes/notes_function.php
+++ scp/includes/notes_function.php
@@ -55,7 +55,7 @@ function auth_img($id, $cid) {
             echo '&nbsp;<a class="bi bi-person-fill-check fs-5 text-black" title="' . $row['contact_notes'] . '" href="https://portal.remoteit.co.uk/client/contacts/?client_id='.$cid.'"></a>&nbsp;';
         }
     }
-	echo '&nbsp;<a class="bi bi-list-check fs-5 text-black" href="https://portal.remoteit.co.uk/client/contacts/?client_id='.$cid.'"></a>&nbsp;';
+	echo '&nbsp;<a class="bi bi-list-check fs-5 text-secondary" href="https://portal.remoteit.co.uk/client/contacts/?client_id='.$cid.'"></a>&nbsp;';
 }
 
 ?>
