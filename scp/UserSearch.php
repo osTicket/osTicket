8df4d451fd5f1525539a7d44ad454e7669c2d0c3
Antt1995 <antt1995@antts.uk>
2025-01-03 09:04:42 +0000

    ✨ Fix datepicker ID for consistency
    Updated the datepicker ID from `cexpiryDate` to `expiryDate` for better clarity and consistency in the code.

--- scp/UserSearch.php
+++ scp/UserSearch.php
@@ -1216,7 +1216,7 @@ body {
 	<script>
 		$('#addCNoteModal').on('shown.bs.modal', function () {
 			$(function() {
-				$("#cexpiryDate").datepicker({
+				$("#expiryDate").datepicker({
 					showButtonPanel: true,
 					dateFormat: 'yy-mm-dd',
 					numberOfMonths: 2,
