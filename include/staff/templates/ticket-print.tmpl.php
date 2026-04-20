f70d37104fa0a0148b8e3673c8abe6c69025c8cc
Valdnet <47037905+Valdnet@users.noreply.github.com>
2026-04-20 14:27:28 +0200

    Change text string to translate for printing

--- include/staff/templates/ticket-print.tmpl.php
+++ include/staff/templates/ticket-print.tmpl.php
@@ -127,12 +127,12 @@ img.avatar {
 <htmlpagefooter name="def" style="display:none">
     <div class="hr">&nbsp;</div>
     <table width="100%"><tr><td class="flush-left">
-        Ticket #<?php echo $ticket->getNumber(); ?> printed by
-        <?php echo $thisstaff->getUserName(); ?> on
+        <?php echo __('Ticket #'); ?><?php echo $ticket->getNumber(); ?> <?php echo __('printed by'); ?>
+        <?php echo $thisstaff->getUserName(); ?> <?php echo __('on'); ?>
         <?php echo Format::daydatetime(Misc::gmtime()); ?>
     </td>
     <td class="flush-right">
-        Page {PAGENO}
+        <?php echo __('Page'); ?> {PAGENO}
     </td>
     </tr></table>
 </htmlpagefooter>
